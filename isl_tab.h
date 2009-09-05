b194d31e48f8b34d74d2a48f861b19c5b5aa6c15
Sven Verdoolaege <skimo@purples.(none)>
2009-09-05 23:28:59 +0200

    privately export isl_tab_kill_col

--- isl_tab.h
+++ isl_tab.h
@@ -211,6 +211,7 @@ void isl_tab_pivot(struct isl_tab *tab, int row, int col);
 int isl_tab_add_row(struct isl_tab *tab, isl_int *line);
 int isl_tab_row_is_redundant(struct isl_tab *tab, int row);
 int isl_tab_min_at_most_neg_one(struct isl_tab *tab, struct isl_tab_var *var);
+int isl_tab_kill_col(struct isl_tab *tab, int col);
 
 void isl_tab_push(struct isl_tab *tab, enum isl_tab_undo_type type);
 void isl_tab_push_var(struct isl_tab *tab,
