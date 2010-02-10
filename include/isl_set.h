711f884433125cb042ec7d83f50cd9bc88ffc364
Sven Verdoolaege <skimo@kotnet.org>
2010-02-10 11:25:25 +0100

    export isl_map_align_divs

--- include/isl_set.h
+++ include/isl_set.h
@@ -258,6 +258,7 @@ int isl_set_is_equal(__isl_keep isl_set *set1, __isl_keep isl_set *set2);
 
 struct isl_set *isl_basic_set_compute_divs(struct isl_basic_set *bset);
 __isl_give isl_set *isl_set_compute_divs(__isl_take isl_set *set);
+__isl_give isl_set *isl_set_align_divs(__isl_take isl_set *set);
 
 struct isl_basic_set *isl_set_copy_basic_set(struct isl_set *set);
 struct isl_set *isl_set_drop_basic_set(struct isl_set *set,
