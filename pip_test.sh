08232ef0e9687f7daa0e122fbe211df055456b72
Sven Verdoolaege <skimo@kotnet.org>
2010-03-10 13:40:08 +0100

    add some pip tests from the piplib distribution

---
+++ pip_test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+PIP_TESTS="\
+	boulet.pip \
+	brisebarre.pip \
+	cg1.pip \
+	esced.pip \
+	ex2.pip \
+	ex.pip \
+	fimmel.pip \
+	max.pip \
+	negative.pip \
+	small.pip \
+	sor1d.pip \
+	square.pip \
+	sven.pip"
+
+for i in $PIP_TESTS; do
+	echo $i;
+	./isl_pip$EXEEXT -T < $srcdir/test_inputs/$i || exit
+done
