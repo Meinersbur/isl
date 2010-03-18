a0467905eb1631fd47f1a982694cc8f514af70c7
Sven Verdoolaege <skimo@kotnet.org>
2010-03-18 14:23:58 +0100

    export isl_map_gist

--- include/isl_map.h
+++ include/isl_map.h
@@ -364,6 +364,8 @@ int isl_map_fast_is_fixed(struct isl_map *map,
 
 __isl_give isl_basic_map *isl_basic_map_gist(__isl_take isl_basic_map *bmap,
 	__isl_take isl_basic_map *context);
+__isl_give isl_map *isl_map_gist(__isl_take isl_map *map,
+	__isl_take isl_basic_map *context);
 
 __isl_give isl_map *isl_map_coalesce(__isl_take isl_map *map);
 
