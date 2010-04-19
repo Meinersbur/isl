f4135171afb43b420f582879ec22bbfed63700f8
Sven Verdoolaege <skimo@kotnet.org>
2010-04-19 11:18:42 +0200

    doc: document *_get_dim functions

--- include/isl_map.h
+++ include/isl_map.h
@@ -122,8 +122,8 @@ unsigned isl_map_n_out(const struct isl_map *map);
 unsigned isl_map_n_param(const struct isl_map *map);
 unsigned isl_map_dim(const struct isl_map *map, enum isl_dim_type type);
 
-struct isl_dim *isl_basic_map_get_dim(struct isl_basic_map *bmap);
-struct isl_dim *isl_map_get_dim(struct isl_map *map);
+__isl_give isl_dim *isl_basic_map_get_dim(__isl_keep isl_basic_map *bmap);
+__isl_give isl_dim *isl_map_get_dim(__isl_keep isl_map *map);
 
 struct isl_basic_map *isl_basic_map_alloc(struct isl_ctx *ctx,
 		unsigned nparam, unsigned in, unsigned out, unsigned extra,
