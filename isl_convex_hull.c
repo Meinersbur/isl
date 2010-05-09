d1374f24acec4c3b3e65f1c6f44b54406500470d
Sven Verdoolaege <skimo@kotnet.org>
2010-05-09 16:06:52 +0200

    privately export isl_set_is_bounded

--- isl_convex_hull.c
+++ isl_convex_hull.c
@@ -869,13 +869,18 @@ static int isl_basic_set_is_bounded(struct isl_basic_set *bset)
 	struct isl_tab *tab;
 	int bounded;
 
+	if (!bset)
+		return -1;
+	if (isl_basic_set_fast_is_empty(bset))
+		return 1;
+
 	tab = isl_tab_from_recession_cone(bset);
 	bounded = isl_tab_cone_is_bounded(tab);
 	isl_tab_free(tab);
 	return bounded;
 }
 
-static int isl_set_is_bounded(struct isl_set *set)
+int isl_set_is_bounded(__isl_keep isl_set *set)
 {
 	int i;
 
