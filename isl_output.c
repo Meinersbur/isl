Meinersbur/isl

commit e0947079c36481d5cc873b207f42057a0e3df9c0

isl_output.c: extract out common print_dim
Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_output.c
@@ -324,6 +324,21 @@ static __isl_give isl_printer *print_tuple(__isl_keep isl_dim *dim,
 	return p;
 }
 
+static __isl_give isl_printer *print_dim(__isl_keep isl_dim *dim,
+	__isl_take isl_printer *p, int set, int latex,
+	__isl_keep isl_basic_map *eq)
+{
+	if (set) {
+		p = print_tuple(dim, p, isl_dim_set, 1, latex, eq);
+	} else {
+		p = print_tuple(dim, p, isl_dim_in, 0, latex, eq);
+		p = isl_printer_print_str(p, s_to[0]);
+		p = print_tuple(dim, p, isl_dim_out, 0, latex, eq);
+	}
+
+	return p;
+}
+
 static __isl_give isl_printer *print_omega_parameters(__isl_keep isl_dim *dim,
 	__isl_take isl_printer *p)
 {
@@ -547,9 +562,7 @@ static __isl_give isl_printer *isl_basic_map_print_isl(
 		p = isl_printer_print_str(p, " -> ");
 	}
 	p = isl_printer_print_str(p, "{ ");
-	p = print_tuple(bmap->dim, p, isl_dim_in, 0, latex, NULL);
-	p = isl_printer_print_str(p, " -> ");
-	p = print_tuple(bmap->dim, p, isl_dim_out, 0, latex, NULL);
+	p = print_dim(bmap->dim, p, 0, latex, NULL);
 	p = isl_printer_print_str(p, " : ");
 	p = print_disjunct(bmap, bmap->dim, p, 0, latex);
 	p = isl_printer_print_str(p, " }");
@@ -568,7 +581,7 @@ static __isl_give isl_printer *isl_basic_set_print_isl(
 		p = isl_printer_print_str(p, " -> ");
 	}
 	p = isl_printer_print_str(p, "{ ");
-	p = print_tuple(bset->dim, p, isl_dim_set, 1, latex, NULL);
+	p = print_dim(bset->dim, p, 1, latex, NULL);
 	p = isl_printer_print_str(p, " : ");
 	p = print_disjunct((isl_basic_map *)bset, bset->dim, p, 1, latex);
 	p = isl_printer_print_str(p, " }");
@@ -748,13 +761,7 @@ static __isl_give isl_printer *print_split_map(__isl_take isl_printer *p,
 		dim = split[i].map->dim;
 		if (i)
 			p = isl_printer_print_str(p, "; ");
-		if (set)
-			p = print_tuple(dim, p, isl_dim_set, 1, 0, split[i].aff);
-		else {
-			p = print_tuple(dim, p, isl_dim_in, 0, 0, split[i].aff);
-			p = isl_printer_print_str(p, s_to[0]);
-			p = print_tuple(dim, p, isl_dim_out, 0, 0, split[i].aff);
-		}
+		p = print_dim(dim, p, set, 0, split[i].aff);
 		p = print_disjuncts(split[i].map, p, set, 0);
 	}
 
@@ -771,13 +778,7 @@ static __isl_give isl_printer *isl_map_print_isl_body(__isl_keep isl_map *map,
 	if (split) {
 		p = print_split_map(p, split, map->n, set);
 	} else {
-		if (set)
-			p = print_tuple(map->dim, p, isl_dim_set, 1, 0, NULL);
-		else {
-			p = print_tuple(map->dim, p, isl_dim_in, 0, 0, NULL);
-			p = isl_printer_print_str(p, s_to[0]);
-			p = print_tuple(map->dim, p, isl_dim_out, 0, 0, NULL);
-		}
+		p = print_dim(map->dim, p, set, 0, NULL);
 		p = print_disjuncts(map, p, set, 0);
 	}
 	free_split(split, map->n);
@@ -805,13 +806,7 @@ static __isl_give isl_printer *print_latex_map(__isl_keep isl_map *map,
 		p = isl_printer_print_str(p, s_to[1]);
 	}
 	p = isl_printer_print_str(p, s_open_set[1]);
-	if (set)
-		p = print_tuple(map->dim, p, isl_dim_set, 1, 1, aff);
-	else {
-		p = print_tuple(map->dim, p, isl_dim_in, 0, 1, aff);
-		p = isl_printer_print_str(p, s_to[1]);
-		p = print_tuple(map->dim, p, isl_dim_out, 0, 1, aff);
-	}
+	p = print_dim(map->dim, p, set, 1, aff);
 	p = print_disjuncts(map, p, set, 1);
 	p = isl_printer_print_str(p, s_close_set[1]);
 
@@ -1308,7 +1303,7 @@ static __isl_give isl_printer *isl_pwqp_print_isl_body(
 			p = isl_printer_print_str(p, "; ");
 		if (isl_dim_size(pwqp->p[i].set->dim, isl_dim_set) > 0 ||
 		    isl_dim_get_tuple_name(pwqp->p[i].set->dim, isl_dim_set)) {
-			p = print_tuple(pwqp->p[i].set->dim, p, isl_dim_set, 1, 0, NULL);
+			p = print_dim(pwqp->p[i].set->dim, p, 1, 0, NULL);
 			p = isl_printer_print_str(p, " -> ");
 		}
 		p = isl_printer_print_qpolynomial(p, pwqp->p[i].qp);
@@ -1331,7 +1326,7 @@ static __isl_give isl_printer *print_pw_qpolynomial_isl(
 	p = isl_printer_print_str(p, "{ ");
 	if (pwqp->n == 0) {
 		if (isl_dim_size(pwqp->dim, isl_dim_set) > 0) {
-			p = print_tuple(pwqp->dim, p, isl_dim_set, 1, 0, NULL);
+			p = print_dim(pwqp->dim, p, 1, 0, NULL);
 			p = isl_printer_print_str(p, " -> ");
 		}
 		p = isl_printer_print_str(p, "0");
@@ -1368,7 +1363,7 @@ static __isl_give isl_printer *isl_pwf_print_isl_body(
 		if (i)
 			p = isl_printer_print_str(p, "; ");
 		if (isl_dim_size(pwf->p[i].set->dim, isl_dim_set) > 0) {
-			p = print_tuple(pwf->p[i].set->dim, p, isl_dim_set, 1, 0, NULL);
+			p = print_dim(pwf->p[i].set->dim, p, 1, 0, NULL);
 			p = isl_printer_print_str(p, " -> ");
 		}
 		p = qpolynomial_fold_print(pwf->p[i].fold, p);
@@ -1388,7 +1383,7 @@ static __isl_give isl_printer *print_pw_qpolynomial_fold_isl(
 	p = isl_printer_print_str(p, "{ ");
 	if (pwf->n == 0) {
 		if (isl_dim_size(pwf->dim, isl_dim_set) > 0) {
-			p = print_tuple(pwf->dim, p, isl_dim_set, 1, 0, NULL);
+			p = print_dim(pwf->dim, p, 1, 0, NULL);
 			p = isl_printer_print_str(p, " -> ");
 		}
 		p = isl_printer_print_str(p, "0");

commit 4a3894473fc79c5c6edd53c473bac51300f14ab6

isl_printer_print_map: fix printing in latex format
It was broken by e094707 (isl_output.c: extract out common print_dim).

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_output.c
@@ -369,7 +369,7 @@ static __isl_give isl_printer *print_nested_map_dim(__isl_take isl_printer *p,
 {
 	p = print_nested_tuple(p, global_dim, global_type,
 			local_dim, isl_dim_in, set, latex, eq, offset);
-	p = isl_printer_print_str(p, s_to[0]);
+	p = isl_printer_print_str(p, s_to[latex]);
 	p = print_nested_tuple(p, global_dim, global_type,
 			local_dim, isl_dim_out, set, latex, eq, offset);
 
@@ -384,7 +384,7 @@ static __isl_give isl_printer *print_dim(__isl_keep isl_dim *dim,
 		p = print_tuple(dim, p, isl_dim_set, 1, latex, eq);
 	else {
 		p = print_tuple(dim, p, isl_dim_in, 0, latex, eq);
-		p = isl_printer_print_str(p, s_to[0]);
+		p = isl_printer_print_str(p, s_to[latex]);
 		p = print_tuple(dim, p, isl_dim_out, 0, latex, eq);
 	}
 