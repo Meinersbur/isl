d17bf627000cd33dc683278914d0c8c7fca02f8b
Sven Verdoolaege <skimo@kotnet.org>
2011-01-23 18:40:21 +0100

    export isl_int_obj_free
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/obj.h
+++ include/isl/obj.h
@@ -48,6 +48,7 @@ struct isl_int_obj;
 typedef struct isl_int_obj isl_int_obj;
 
 __isl_give isl_int_obj *isl_int_obj_alloc(isl_ctx *ctx, isl_int v);
+void isl_int_obj_free(__isl_take isl_int_obj *i);
 __isl_give isl_int_obj *isl_int_obj_add(__isl_take isl_int_obj *i1,
 	__isl_take isl_int_obj *i2);
 __isl_give isl_int_obj *isl_int_obj_sub(__isl_take isl_int_obj *i1,
