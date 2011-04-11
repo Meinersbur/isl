Meinersbur/isl

commit f1ea969113607181555328e62e2825bcbb26bfc0

isl_stream_read_map: treat divs as output variables while adding constraints
This will allow us to more easily handle other extra variables.

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_input.c
@@ -897,6 +897,40 @@ static int is_comparator(struct isl_token *tok)
 	}
 }
 
+/* Add any variables in the variable list "v" that are not already in "bmap"
+ * as output variables in "bmap".
+ */
+static __isl_give isl_basic_map *add_lifted_divs(__isl_take isl_basic_map *bmap,
+	struct vars *v)
+{
+	int i;
+	int extra;
+	struct variable *var;
+
+	extra = v->n - isl_basic_map_total_dim(bmap);
+
+	if (extra == 0)
+		return bmap;
+
+	bmap = isl_basic_map_add(bmap, isl_dim_out, extra);
+	bmap = isl_basic_map_extend_dim(bmap, isl_basic_map_get_dim(bmap),
+					0, 0, 2 * extra);
+
+	for (i = 0, var = v->v; i < extra; ++i, var = var->next) {
+		var->def = isl_vec_zero_extend(var->def, 2 + v->n);
+		if (!var->def)
+			goto error;
+		if (isl_basic_map_add_div_constraints_var(bmap, var->pos,
+							  var->def->el) < 0)
+			goto error;
+	}
+
+	return bmap;
+error:
+	isl_basic_map_free(bmap);
+	return NULL;
+}
+
 static struct isl_basic_map *add_constraint(struct isl_stream *s,
 	struct vars *v, struct isl_basic_map *bmap)
 {
@@ -925,7 +959,7 @@ static struct isl_basic_map *add_constraint(struct isl_stream *s,
 		aff1 = isl_mat_add_zero_cols(aff1, aff2->n_col - aff1->n_col);
 		if (!aff1)
 			goto error;
-		bmap = add_divs(bmap, v);
+		bmap = add_lifted_divs(bmap, v);
 		bmap = isl_basic_map_extend_constraints(bmap, 0,
 						aff1->n_row * aff2->n_row);
 		for (i = 0; i < aff1->n_row; ++i)
@@ -963,9 +997,13 @@ static isl_map *read_constraint(struct isl_stream *s,
 	if (!bmap)
 		return NULL;
 
+	bmap = isl_basic_set_unwrap(isl_basic_set_lift(isl_basic_map_wrap(bmap)));
+
 	bmap = add_constraint(s, v, bmap);
 	bmap = isl_basic_map_simplify(bmap);
 	bmap = isl_basic_map_finalize(bmap);
+
+	bmap = isl_basic_set_unwrap(isl_basic_map_domain(bmap));
 
 	vars_drop(v, v->n - n);
 

commit 91e50bc447125cd4a64416ed64678303ac3152f2

isl_stream_read_map: fix handling of divs in tuples
Supoprt for them was broken in f1ea969 (isl_stream_read_map: treat divs
as output variables while adding constraints, Thu Mar 17 16:29:06 2011 +0100).
When treating divs as output variables, we need to make sure that
each of them is backed by a variable in the list of active variables.

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_input.c
@@ -1254,11 +1254,16 @@ static isl_map *read_constraint(struct isl_stream *s,
 {
 	int n = v->n;
 	isl_map *map;
+	unsigned total;
 
 	if (!bmap)
 		return NULL;
 
 	bmap = isl_basic_set_unwrap(isl_basic_set_lift(isl_basic_map_wrap(bmap)));
+	total = isl_basic_map_total_dim(bmap);
+	while (v->n < total)
+		if (vars_add_anon(v) < 0)
+			goto error;
 
 	bmap = add_constraint(s, v, bmap);
 	bmap = isl_basic_map_simplify(bmap);
@@ -1273,6 +1278,9 @@ static isl_map *read_constraint(struct isl_stream *s,
 	vars_drop(v, v->n - n);
 
 	return map;
+error:
+	isl_basic_map_free(bmap);
+	return NULL;
 }
 
 static struct isl_map *read_disjuncts(struct isl_stream *s,

isl_test.c
@@ -102,6 +102,7 @@ void test_parse(struct isl_ctx *ctx)
 	test_parse_map_equal(ctx, str, str2);
 
 	test_parse_pwqp(ctx, "{ [i] -> i + [ (i + [i/3])/2 ] }");
+	test_parse_map(ctx, "{ S1[i] -> [([i/10]),i%10] : 0 <= i <= 45 }");
 }
 
 void test_read(struct isl_ctx *ctx)