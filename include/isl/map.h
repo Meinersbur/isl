abf2ace90ca4bae95f90bb7bbad4a379b13914e8
Sven Verdoolaege <skimo@kotnet.org>
2011-06-12 11:18:11 +0200

    export isl_basic_map_flatten_range
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/map.h
+++ include/isl/map.h
@@ -353,6 +353,8 @@ __isl_give isl_basic_map *isl_basic_set_unwrap(__isl_take isl_basic_set *bset);
 __isl_give isl_map *isl_set_unwrap(__isl_take isl_set *set);
 __isl_give isl_basic_map *isl_basic_map_flatten(__isl_take isl_basic_map *bmap);
 __isl_give isl_map *isl_map_flatten(__isl_take isl_map *map);
+__isl_give isl_basic_map *isl_basic_map_flatten_range(
+	__isl_take isl_basic_map *bmap);
 __isl_give isl_map *isl_map_flatten_range(__isl_take isl_map *map);
 __isl_give isl_basic_set *isl_basic_set_flatten(__isl_take isl_basic_set *bset);
 __isl_give isl_set *isl_set_flatten(__isl_take isl_set *set);
