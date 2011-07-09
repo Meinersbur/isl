aff456562e48d4d779f2b0cf80bba2375a54a8a1
Sven Verdoolaege <skimo@kotnet.org>
2011-07-09 11:52:59 +0200

    export isl_pw_aff_nonneg_set
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -127,6 +127,8 @@ __isl_give isl_pw_aff *isl_pw_aff_gist(__isl_take isl_pw_aff *pwaff,
 
 __isl_give isl_map *isl_map_from_pw_aff(__isl_take isl_pw_aff *pwaff);
 
+__isl_give isl_set *isl_pw_aff_nonneg_set(__isl_take isl_pw_aff *pwaff);
+
 __isl_give isl_set *isl_pw_aff_lt_set(__isl_take isl_pw_aff *pwaff1,
 	__isl_take isl_pw_aff *pwaff2);
 __isl_give isl_set *isl_pw_aff_ge_set(__isl_take isl_pw_aff *pwaff1,
