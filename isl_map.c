e58a8e14105f6e9218a64096c3e279e690e2f7e0
Sven Verdoolaege <skimo@kotnet.org>
2011-07-25 17:17:12 +0200

    isl_map_intersect: be careful when intersecting with empty parameter domain
    isl_map_intersect currently doubles as a isl_map_intersect_params
    and in this capacity, the spaces of the two arguments may be different.
    In this case, we need to make sure that we return an (empty) map
    in the correct space.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_map.c
+++ isl_map.c
@@ -2268,11 +2268,13 @@ struct isl_map *isl_map_intersect(struct isl_map *map1, struct isl_map *map2)
 	if (!map1 || !map2)
 		goto error;
 
-	if (isl_map_plain_is_empty(map1)) {
+	if (isl_map_plain_is_empty(map1) &&
+	    isl_dim_equal(map1->dim, map2->dim)) {
 		isl_map_free(map2);
 		return map1;
 	}
-	if (isl_map_plain_is_empty(map2)) {
+	if (isl_map_plain_is_empty(map2) &&
+	    isl_dim_equal(map1->dim, map2->dim)) {
 		isl_map_free(map1);
 		return map2;
 	}
