2b74eb8e22c7794eac79cf53f5611dd7df84001d
Sven Verdoolaege <skimo@kotnet.org>
2012-01-03 10:35:43 +0100

    add isl_aff_project_domain_on_params
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -83,6 +83,7 @@ __isl_give isl_aff *isl_aff_add_dims(__isl_take isl_aff *aff,
 	enum isl_dim_type type, unsigned n);
 __isl_give isl_aff *isl_aff_drop_dims(__isl_take isl_aff *aff,
 	enum isl_dim_type type, unsigned first, unsigned n);
+__isl_give isl_aff *isl_aff_project_domain_on_params(__isl_take isl_aff *aff);
 
 __isl_give isl_aff *isl_aff_align_params(__isl_take isl_aff *aff,
 	__isl_take isl_space *model);
