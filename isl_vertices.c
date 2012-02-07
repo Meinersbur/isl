Meinersbur/isl

commit 328c78f7d2fdc3068a2a7232149d6d0b35a35c8d

isl_tab_from_basic_map: preserve all constraints in input when tracking
If the user wants to keep track of a basic map representation of the tableau,
then we need to make sure that the initial tableau corresponds exactly to
the input.  In particular, we need to make sure that no (redundant) constraints
are removed.

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_vertices.c
@@ -422,7 +422,7 @@ __isl_give isl_vertices *isl_basic_set_compute_vertices(
 	if (!bset)
 		return NULL;
 
-	tab = isl_tab_from_basic_set(bset);
+	tab = isl_tab_from_basic_set(bset, 0);
 	if (!tab)
 		goto error;
 	tab->strict_redundant = 1;
@@ -876,12 +876,11 @@ static __isl_give isl_vertices *compute_chambers(__isl_take isl_basic_set *bset,
 
 	bset = isl_basic_set_params(bset);
 
-	tab = isl_tab_from_basic_set(bset);
+	tab = isl_tab_from_basic_set(bset, 1);
+	isl_basic_set_free(bset);
 	for (i = 0; i < bset->n_ineq; ++i)
 		if (isl_tab_freeze_constraint(tab, i) < 0)
 			goto error;
-	if (isl_tab_track_bset(tab, bset) < 0)
-		goto error;
 
 	snap = isl_tab_snap(tab);
 

basis_reduction_templ.c
@@ -329,7 +329,7 @@ struct isl_mat *isl_basic_set_reduced_basis(struct isl_basic_set *bset)
 		isl_die(bset->ctx, isl_error_invalid,
 			"no parameters allowed", return NULL);
 
-	tab = isl_tab_from_basic_set(bset);
+	tab = isl_tab_from_basic_set(bset, 0);
 	if (!tab)
 		return NULL;
 

isl_affine_hull.c
@@ -34,7 +34,7 @@ struct isl_basic_map *isl_basic_map_implicit_equalities(
 	if (bmap->n_ineq <= 1)
 		return bmap;
 
-	tab = isl_tab_from_basic_map(bmap);
+	tab = isl_tab_from_basic_map(bmap, 0);
 	if (isl_tab_detect_implicit_equalities(tab) < 0)
 		goto error;
 	bmap = isl_basic_map_update_from_tab(bmap, tab);
@@ -480,7 +480,7 @@ static struct isl_basic_set *uset_affine_hull_bounded(struct isl_basic_set *bset
 		}
 	}
 
-	tab = isl_tab_from_basic_set(bset);
+	tab = isl_tab_from_basic_set(bset, 1);
 	if (!tab)
 		goto error;
 	if (tab->empty) {
@@ -488,8 +488,6 @@ static struct isl_basic_set *uset_affine_hull_bounded(struct isl_basic_set *bset
 		isl_vec_free(sample);
 		return isl_basic_set_set_to_empty(bset);
 	}
-	if (isl_tab_track_bset(tab, isl_basic_set_copy(bset)) < 0)
-		goto error;
 
 	if (!sample) {
 		struct isl_tab_undo *snap;

isl_coalesce.c
@@ -223,7 +223,7 @@ static int fuse(struct isl_map *map, int i, int j,
 	    ISL_F_ISSET(map->p[j], ISL_BASIC_MAP_RATIONAL))
 		ISL_F_SET(fused, ISL_BASIC_MAP_RATIONAL);
 
-	fused_tab = isl_tab_from_basic_map(fused);
+	fused_tab = isl_tab_from_basic_map(fused, 0);
 	if (isl_tab_detect_redundant(fused_tab) < 0)
 		goto error;
 
@@ -1242,7 +1242,7 @@ struct isl_map *isl_map_coalesce(struct isl_map *map)
 
 	n = map->n;
 	for (i = 0; i < map->n; ++i) {
-		tabs[i] = isl_tab_from_basic_map(map->p[i]);
+		tabs[i] = isl_tab_from_basic_map(map->p[i], 0);
 		if (!tabs[i])
 			goto error;
 		if (!ISL_F_ISSET(map->p[i], ISL_BASIC_MAP_NO_IMPLICIT))

isl_convex_hull.c
@@ -94,7 +94,7 @@ __isl_give isl_basic_map *isl_basic_map_remove_redundancies(
 	if (bmap->n_ineq <= 1)
 		return bmap;
 
-	tab = isl_tab_from_basic_map(bmap);
+	tab = isl_tab_from_basic_map(bmap, 0);
 	if (isl_tab_detect_implicit_equalities(tab) < 0)
 		goto error;
 	if (isl_tab_detect_redundant(tab) < 0)
@@ -1169,7 +1169,7 @@ static struct isl_vec *valid_direction(
 		goto error;
 	lp = valid_direction_lp(isl_basic_set_copy(bset1),
 				isl_basic_set_copy(bset2));
-	tab = isl_tab_from_basic_set(lp);
+	tab = isl_tab_from_basic_set(lp, 0);
 	sample = isl_tab_get_sample_value(tab);
 	isl_tab_free(tab);
 	isl_basic_set_free(lp);
@@ -2117,7 +2117,7 @@ static int is_bound(struct sh_data *data, struct isl_set *set, int j,
 	isl_int opt;
 
 	if (!data->p[j].tab) {
-		data->p[j].tab = isl_tab_from_basic_set(set->p[j]);
+		data->p[j].tab = isl_tab_from_basic_set(set->p[j], 0);
 		if (!data->p[j].tab)
 			return -1;
 	}

isl_lp.c
@@ -28,7 +28,7 @@ enum isl_lp_result isl_tab_solve_lp(struct isl_basic_map *bmap, int maximize,
 		isl_seq_neg(f, f, 1 + dim);
 
 	bmap = isl_basic_map_gauss(bmap, NULL);
-	tab = isl_tab_from_basic_map(bmap);
+	tab = isl_tab_from_basic_map(bmap, 0);
 	res = isl_tab_min(tab, f, denom, opt, opt_denom, 0);
 	if (res == isl_lp_ok && sol) {
 		*sol = isl_tab_get_sample_value(tab);

isl_map.c
@@ -8730,7 +8730,7 @@ int isl_basic_set_vars_get_sign(__isl_keep isl_basic_set *bset,
 		return -1;
 
 	bound = isl_vec_alloc(bset->ctx, 1 + isl_basic_set_total_dim(bset));
-	tab = isl_tab_from_basic_set(bset);
+	tab = isl_tab_from_basic_set(bset, 0);
 	if (!bound || !tab)
 		goto error;
 

isl_map_simplify.c
@@ -1579,7 +1579,7 @@ static __isl_give isl_basic_set *uset_gist_full(__isl_take isl_basic_set *bset,
 	context_ineq = context->n_ineq;
 	combined = isl_basic_set_cow(isl_basic_set_copy(context));
 	combined = isl_basic_set_extend_constraints(combined, 0, bset->n_ineq);
-	tab = isl_tab_from_basic_set(combined);
+	tab = isl_tab_from_basic_set(combined, 0);
 	for (i = 0; i < context_ineq; ++i)
 		if (isl_tab_freeze_constraint(tab, i) < 0)
 			goto error;
@@ -2207,7 +2207,7 @@ static struct isl_basic_map *drop_more_redundant_divs(
 	if (!vec)
 		goto error;
 
-	tab = isl_tab_from_basic_map(bmap);
+	tab = isl_tab_from_basic_map(bmap, 0);
 
 	while (n > 0) {
 		int i, l, u;

commit def284dda95bb5ac6714c5b782fc32d182a61257

isl_vertices.c: compute_chambers: avoid access to freed data structure
The problem was introduced in 328c78f (isl_tab_from_basic_map: preserve all
constraints in input when tracking, Mon Jan 16 16:55:44 2012 +0100).

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_vertices.c
@@ -877,10 +877,10 @@ static __isl_give isl_vertices *compute_chambers(__isl_take isl_basic_set *bset,
 	bset = isl_basic_set_params(bset);
 
 	tab = isl_tab_from_basic_set(bset, 1);
-	isl_basic_set_free(bset);
 	for (i = 0; i < bset->n_ineq; ++i)
 		if (isl_tab_freeze_constraint(tab, i) < 0)
 			goto error;
+	isl_basic_set_free(bset);
 
 	snap = isl_tab_snap(tab);
 