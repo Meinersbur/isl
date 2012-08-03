a703344ebbe564f91f0c1d4709e3ae396a3a2ceb
Sven Verdoolaege <skimo@kotnet.org>
2012-08-03 12:34:46 +0200

    declare isl_pw_multi_aff_project_domain_on_params
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -385,6 +385,9 @@ __isl_give isl_pw_multi_aff *isl_pw_multi_aff_intersect_params(
 __isl_give isl_pw_multi_aff *isl_pw_multi_aff_intersect_domain(
 	__isl_take isl_pw_multi_aff *pma, __isl_take isl_set *set);
 
+__isl_give isl_pw_multi_aff *isl_pw_multi_aff_project_domain_on_params(
+	__isl_take isl_pw_multi_aff *pma);
+
 __isl_give isl_pw_multi_aff *isl_pw_multi_aff_coalesce(
 	__isl_take isl_pw_multi_aff *pma);
 __isl_give isl_pw_multi_aff *isl_pw_multi_aff_gist_params(
