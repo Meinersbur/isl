385d67ab75e5791d7c548f23fa933717c16fb6ac
Sven Verdoolaege <skimo@kotnet.org>
2012-08-20 14:44:17 +0200

    declare isl_basic_map_remove_divs_involving_dims
    This function was added in 720f32e (add isl_set_remove_divs_involving_dims,
    Wed Oct 27 13:04:04 2010 +0200), but was not exported.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/map.h
+++ include/isl/map.h
@@ -430,6 +430,9 @@ __isl_give isl_map *isl_map_eliminate(__isl_take isl_map *map,
 	enum isl_dim_type type, unsigned first, unsigned n);
 __isl_give isl_map *isl_map_remove_dims(__isl_take isl_map *map,
 	enum isl_dim_type type, unsigned first, unsigned n);
+__isl_give isl_basic_map *isl_basic_map_remove_divs_involving_dims(
+	__isl_take isl_basic_map *bmap,
+	enum isl_dim_type type, unsigned first, unsigned n);
 __isl_give isl_map *isl_map_remove_divs_involving_dims(__isl_take isl_map *map,
 	enum isl_dim_type type, unsigned first, unsigned n);
 struct isl_map *isl_map_remove_inputs(struct isl_map *map,
