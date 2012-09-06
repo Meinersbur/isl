Meinersbur/isl

commit 387317ac4151ad0048280d6e8992b0ff4fa81ae4

isl_args_parse: optionally ignore --help
Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_arg.c
@@ -1144,6 +1144,23 @@ static int next_arg(struct isl_arg *arg, int a)
 	return -1;
 }
 
+/* Unless ISL_ARG_SKIP_HELP is set, check if any of the arguments is
+ * equal to "--help" and if so call print_help_and_exit.
+ */
+void check_help(struct isl_args *args, int argc, char **argv, void *opt,
+	unsigned flags)
+{
+	int i;
+
+	if (ISL_FL_ISSET(flags, ISL_ARG_SKIP_HELP))
+		return;
+
+	for (i = 1; i < argc; ++i) {
+		if (strcmp(argv[i], "--help") == 0)
+			print_help_and_exit(args->args, argv[0], opt);
+	}
+}
+
 int isl_args_parse(struct isl_args *args, int argc, char **argv, void *opt,
 	unsigned flags)
 {
@@ -1154,10 +1171,7 @@ int isl_args_parse(struct isl_args *args, int argc, char **argv, void *opt,
 
 	n = n_arg(args->args);
 
-	for (i = 1; i < argc; ++i) {
-		if (strcmp(argv[i], "--help") == 0)
-			print_help_and_exit(args->args, argv[0], opt);
-	}
+	check_help(args, argc, argv, opt, flags);
 
 	for (i = 1; i < argc; ++i) {
 		if ((strcmp(argv[i], "--version") == 0 ||

include/isl/arg.h
@@ -274,7 +274,8 @@ struct isl_args {
 	.u = { .version = { .print_version = print } }			\
 },
 
-#define ISL_ARG_ALL	(1 << 0)
+#define ISL_ARG_ALL		(1 << 0)
+#define ISL_ARG_SKIP_HELP	(1 << 1)
 
 void isl_args_set_defaults(struct isl_args *args, void *opt);
 void isl_args_free(struct isl_args *args, void *opt);

commit 0751edfa567165d5a86f939793582c80e6788a7d

hide internal check_help
This function was exposed by mistake when it was introduced in 387317a
(isl_args_parse: optionally ignore --help, Thu Mar 22 17:03:47 2012 +0100).

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_arg.c
@@ -1147,7 +1147,7 @@ static int next_arg(struct isl_arg *arg, int a)
 /* Unless ISL_ARG_SKIP_HELP is set, check if any of the arguments is
  * equal to "--help" and if so call print_help_and_exit.
  */
-void check_help(struct isl_args *args, int argc, char **argv, void *opt,
+static void check_help(struct isl_args *args, int argc, char **argv, void *opt,
 	unsigned flags)
 {
 	int i;