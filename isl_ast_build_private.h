076a978c4331b27eee3260851b3aaa27ea4e469a
Sven Verdoolaege <skimo@kotnet.org>
2013-02-19 16:46:33 +0100

    isl_ast_codegen.c: generate_domain: avoid infinite recursion
    generate_domain checks if the inverse schedule is single-valued
    so that the inverse schedule can be extended if needed.
    Since this check is performed on the gisted inverse schedule,
    it may fail even in a recursive call on an inverse schedule
    that has been extended to ensure that it is single valued.
    We may then end up in an infinite recursion.
    
    To avoid this problem, we keep track of the fact that we have
    already extended the inverse schedule to not be single-valued.
    In such cases, we revert to the ungisted inverse schedule
    if the gisted inverse schedule turns out not to be single-valued.
    
    Reported-by: Tobias Grosser <tobias@grosser.es>
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_ast_build_private.h
+++ isl_ast_build_private.h
@@ -109,6 +109,13 @@ enum isl_ast_build_domain_type {
  * It is currently only used in isl_ast_build_get_schedule, which is
  * in turn only used by user code from within a callback.
  * The value is set right before we may be calling such a callback.
+ *
+ * "single_valued" is set if the current inverse schedule (which may or may
+ * not be stored in "executed") is known to be single valued, specifically
+ * an inverse schedule that was not (appeared not to be) single valued
+ * is extended to a single valued inverse schedule.  This is mainly used
+ * to avoid an infinite recursion when we fail to detect later on that
+ * the extended inverse schedule is single valued.
  */
 struct isl_ast_build {
 	int ref;
@@ -150,6 +157,7 @@ struct isl_ast_build {
 	void *create_leaf_user;
 
 	isl_union_map *executed;
+	int single_valued;
 };
 
 __isl_give isl_ast_build *isl_ast_build_clear_local_info(
@@ -179,6 +187,8 @@ __isl_give isl_ast_build *isl_ast_build_include_stride(
 __isl_give isl_ast_build *isl_ast_build_set_executed(
 	__isl_take isl_ast_build *build,
 	__isl_take isl_union_map *executed);
+__isl_give isl_ast_build *isl_ast_build_set_single_valued(
+	__isl_take isl_ast_build *build, int sv);
 __isl_give isl_set *isl_ast_build_get_domain(
 	__isl_keep isl_ast_build *build);
 __isl_give isl_ast_build *isl_ast_build_restrict_generated(
