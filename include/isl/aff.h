8b00ae084d8c329bcec9cd7714edd545a35914a7
Sven Verdoolaege <skimo@kotnet.org>
2013-06-21 15:00:21 +0200

    declare isl_multi_pw_aff_gist
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -529,6 +529,9 @@ __isl_give isl_multi_pw_aff *isl_multi_pw_aff_identity(
 __isl_give isl_multi_pw_aff *isl_multi_pw_aff_from_pw_aff(
 	__isl_take isl_pw_aff *pa);
 
+__isl_give isl_multi_pw_aff *isl_multi_pw_aff_gist(
+	__isl_take isl_multi_pw_aff *mpa, __isl_take isl_set *set);
+
 __isl_give isl_multi_pw_aff *isl_multi_pw_aff_move_dims(
 	__isl_take isl_multi_pw_aff *pma,
 	enum isl_dim_type dst_type, unsigned dst_pos,
