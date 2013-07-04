1f26863feb3ed68d4b4a7de437af55e5871e8314
Sven Verdoolaege <skimo@kotnet.org>
2013-07-04 12:03:45 +0200

    isl_multi_aff_{add,sub}: align parameters
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_aff.c
+++ isl_aff.c
@@ -3264,10 +3264,33 @@ __isl_give isl_pw_multi_aff *isl_pw_multi_aff_identity(
 	return isl_pw_multi_aff_from_multi_aff(isl_multi_aff_identity(space));
 }
 
-__isl_give isl_multi_aff *isl_multi_aff_add(__isl_take isl_multi_aff *maff1,
-	__isl_take isl_multi_aff *maff2)
+/* Add "ma2" to "ma1" and return the result.
+ *
+ * The parameters of "ma1" and "ma2" are assumed to have been aligned.
+ */
+static __isl_give isl_multi_aff *isl_multi_aff_add_aligned(
+	__isl_take isl_multi_aff *maff1, __isl_take isl_multi_aff *maff2)
 {
 	return isl_multi_aff_bin_op(maff1, maff2, &isl_aff_add);
+}
+
+/* Add "ma2" to "ma1" and return the result.
+ */
+__isl_give isl_multi_aff *isl_multi_aff_add(__isl_take isl_multi_aff *ma1,
+	__isl_take isl_multi_aff *ma2)
+{
+	return isl_multi_aff_align_params_multi_multi_and(ma1, ma2,
+						&isl_multi_aff_add_aligned);
+}
+
+/* Subtract "ma2" from "ma1" and return the result.
+ *
+ * The parameters of "ma1" and "ma2" are assumed to have been aligned.
+ */
+static __isl_give isl_multi_aff *isl_multi_aff_sub_aligned(
+	__isl_take isl_multi_aff *ma1, __isl_take isl_multi_aff *ma2)
+{
+	return isl_multi_aff_bin_op(ma1, ma2, &isl_aff_sub);
 }
 
 /* Subtract "ma2" from "ma1" and return the result.
@@ -3275,7 +3298,8 @@ __isl_give isl_multi_aff *isl_multi_aff_add(__isl_take isl_multi_aff *maff1,
 __isl_give isl_multi_aff *isl_multi_aff_sub(__isl_take isl_multi_aff *ma1,
 	__isl_take isl_multi_aff *ma2)
 {
-	return isl_multi_aff_bin_op(ma1, ma2, &isl_aff_sub);
+	return isl_multi_aff_align_params_multi_multi_and(ma1, ma2,
+						&isl_multi_aff_sub_aligned);
 }
 
 /* Given two multi-affine expressions A -> B and C -> D,
