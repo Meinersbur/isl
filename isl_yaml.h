7c3b135a381ba6012c8050f39379d97b974088c8
Sven Verdoolaege <skimo@kotnet.org>
2013-10-03 11:41:01 +0200

    add basic support for YAML printing
    We will need this to print schedule trees.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_yaml.h
+++ isl_yaml.h
@@ -5,9 +5,12 @@
 
 enum isl_yaml_state {
 	isl_yaml_none,
+	isl_yaml_mapping_first_key_start,
 	isl_yaml_mapping_key_start,
 	isl_yaml_mapping_key,
+	isl_yaml_mapping_val_start,
 	isl_yaml_mapping_val,
+	isl_yaml_sequence_first_start,
 	isl_yaml_sequence_start,
 	isl_yaml_sequence
 };
