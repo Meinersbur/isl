692e360327cbfff140c23c3e0b341fa0254859ab
Sven Verdoolaege <skimo@kotnet.org>
2014-01-10 09:35:07 +0100

    add isl_space_factor_range
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_space.c
+++ isl_space.c
@@ -1271,6 +1271,15 @@ error:
 	return NULL;
 }
 
+/* Given a space of the form [A -> B] -> [C -> D], return the space B -> D.
+ */
+__isl_give isl_space *isl_space_factor_range(__isl_take isl_space *space)
+{
+	space = isl_space_domain_factor_range(space);
+	space = isl_space_range_factor_range(space);
+	return space;
+}
+
 /* Given a space of the form [A -> B] -> C, return the space A -> C.
  */
 __isl_give isl_space *isl_space_domain_factor_domain(
