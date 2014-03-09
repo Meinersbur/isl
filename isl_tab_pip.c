a9e989f52802ce6e2ca98eef4bf131ffdb0b2634
Tobias Grosser <tobias@grosser.es>
2014-03-10 00:41:45 +0100

    Do not dereference a NULL pointer in tab_for_lexmin
    When using the compute-out feature, we may return a tab that is NULL.
    By caching the offset and the length of the sequence we want to negate,
    we avoid a segfault that we previously run into when deriving values
    for the number of dimensions from the tab due to the tab pointer being
    NULL.
    
    Signed-off-by: Tobias Grosser <tobias@grosser.es>
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_tab_pip.c
+++ isl_tab_pip.c
@@ -2075,6 +2075,8 @@ static struct isl_tab *tab_for_lexmin(struct isl_basic_map *bmap,
 {
 	int i;
 	struct isl_tab *tab;
+	unsigned n_var;
+	unsigned o_var;
 
 	tab = isl_tab_alloc(bmap->ctx, 2 * bmap->n_eq + bmap->n_ineq + 1,
 			    isl_basic_map_total_dim(bmap), M);
@@ -2100,16 +2102,16 @@ static struct isl_tab *tab_for_lexmin(struct isl_basic_map *bmap,
 		tab->var[i].is_nonneg = 1;
 		tab->var[i].frozen = 1;
 	}
+	o_var = 1 + tab->n_param;
+	n_var = tab->n_var - tab->n_param - tab->n_div;
 	for (i = 0; i < bmap->n_eq; ++i) {
 		if (max)
-			isl_seq_neg(bmap->eq[i] + 1 + tab->n_param,
-				    bmap->eq[i] + 1 + tab->n_param,
-				    tab->n_var - tab->n_param - tab->n_div);
+			isl_seq_neg(bmap->eq[i] + o_var,
+				    bmap->eq[i] + o_var, n_var);
 		tab = add_lexmin_valid_eq(tab, bmap->eq[i]);
 		if (max)
-			isl_seq_neg(bmap->eq[i] + 1 + tab->n_param,
-				    bmap->eq[i] + 1 + tab->n_param,
-				    tab->n_var - tab->n_param - tab->n_div);
+			isl_seq_neg(bmap->eq[i] + o_var,
+				    bmap->eq[i] + o_var, n_var);
 		if (!tab || tab->empty)
 			return tab;
 	}
@@ -2117,14 +2119,12 @@ static struct isl_tab *tab_for_lexmin(struct isl_basic_map *bmap,
 		goto error;
 	for (i = 0; i < bmap->n_ineq; ++i) {
 		if (max)
-			isl_seq_neg(bmap->ineq[i] + 1 + tab->n_param,
-				    bmap->ineq[i] + 1 + tab->n_param,
-				    tab->n_var - tab->n_param - tab->n_div);
+			isl_seq_neg(bmap->ineq[i] + o_var,
+				    bmap->ineq[i] + o_var, n_var);
 		tab = add_lexmin_ineq(tab, bmap->ineq[i]);
 		if (max)
-			isl_seq_neg(bmap->ineq[i] + 1 + tab->n_param,
-				    bmap->ineq[i] + 1 + tab->n_param,
-				    tab->n_var - tab->n_param - tab->n_div);
+			isl_seq_neg(bmap->ineq[i] + o_var,
+				    bmap->ineq[i] + o_var, n_var);
 		if (!tab || tab->empty)
 			return tab;
 	}
