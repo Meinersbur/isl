2e7a8dea8d481b36d2f5be4b219959cd1995b06f
Sven Verdoolaege <skimo@kotnet.org>
2014-04-12 10:15:40 +0200

    isl_tab_pip.c: sol_for_free: handle NULL input
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_tab_pip.c
+++ isl_tab_pip.c
@@ -4753,6 +4753,8 @@ struct isl_sol_for {
 
 static void sol_for_free(struct isl_sol_for *sol_for)
 {
+	if (!sol_for)
+		return;
 	if (sol_for->sol.context)
 		sol_for->sol.context->op->free(sol_for->sol.context);
 	free(sol_for);
