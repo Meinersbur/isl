0f03382151c2d3bb4235c747f259bcdcf499f4ca
Sven Verdoolaege <skimo@kotnet.org>
2014-05-26 16:27:08 +0200

    isl_printer_print_pw_aff: keep track of whether we have printed any constraint
    In the original code, it was the responsibility of print_basic_set_c to
    keep track of whether it has printed any constraints with print_constraint_c
    (or otherwise).
    However, in d593762 (isl_printer_print_pw_aff: skip constraints implied
    by div expression, Tue Feb 7 12:17:03 2012 +0100), we started skipping
    some constraints in print_constraint_c.  If this happened to be the
    first constraint that we tried to print, then print_basic_set_c's notion
    of whether we have printed anything yet would be wrong, resulting in
    output that starts with "&&".
    Keep track of whether we have printed any constraint in print_constraint_c
    itself.
    
    It is difficult to construct a simple test case that reliably tickles
    this bug.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_output.c
+++ isl_output.c
@@ -1678,10 +1678,13 @@ static __isl_give isl_printer *print_affine_c(__isl_take isl_printer *p,
 }
 
 /* We skip the constraint if it is implied by the div expression.
+ *
+ * *first indicates whether this is the first constraint in the conjunction and
+ * is updated if the constraint is actually printed.
  */
 static __isl_give isl_printer *print_constraint_c(__isl_take isl_printer *p,
 	__isl_keep isl_space *dim,
-	__isl_keep isl_basic_set *bset, isl_int *c, const char *op, int first)
+	__isl_keep isl_basic_set *bset, isl_int *c, const char *op, int *first)
 {
 	unsigned o_div;
 	unsigned n_div;
@@ -1693,13 +1696,16 @@ static __isl_give isl_printer *print_constraint_c(__isl_take isl_printer *p,
 	if (div >= 0 && isl_basic_set_is_div_constraint(bset, c, div))
 		return p;
 
-	if (!first)
+	if (!*first)
 		p = isl_printer_print_str(p, " && ");
 
 	p = print_affine_c(p, dim, bset, c);
 	p = isl_printer_print_str(p, " ");
 	p = isl_printer_print_str(p, op);
 	p = isl_printer_print_str(p, " 0");
+
+	*first = 0;
+
 	return p;
 }
 
@@ -1707,6 +1713,7 @@ static __isl_give isl_printer *print_basic_set_c(__isl_take isl_printer *p,
 	__isl_keep isl_space *dim, __isl_keep isl_basic_set *bset)
 {
 	int i, j;
+	int first = 1;
 	unsigned n_div = isl_basic_set_dim(bset, isl_dim_div);
 	unsigned total = isl_basic_set_total_dim(bset) - n_div;
 
@@ -1714,7 +1721,7 @@ static __isl_give isl_printer *print_basic_set_c(__isl_take isl_printer *p,
 		j = isl_seq_last_non_zero(bset->eq[i] + 1 + total, n_div);
 		if (j < 0)
 			p = print_constraint_c(p, dim, bset,
-						bset->eq[i], "==", !i);
+						bset->eq[i], "==", &first);
 		else {
 			if (i)
 				p = isl_printer_print_str(p, " && ");
@@ -1725,11 +1732,12 @@ static __isl_give isl_printer *print_basic_set_c(__isl_take isl_printer *p,
 			p = isl_printer_print_isl_int(p,
 						bset->eq[i][1 + total + j]);
 			p = isl_printer_print_str(p, " == 0");
+			first = 0;
 		}
 	}
 	for (i = 0; i < bset->n_ineq; ++i)
 		p = print_constraint_c(p, dim, bset, bset->ineq[i], ">=",
-					!bset->n_eq && !i);
+					&first);
 	return p;
 }
 
