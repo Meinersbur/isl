1a54c1ace1dc9e084c918046f44b3bceebf0c076
Sven Verdoolaege <skimo@kotnet.org>
2014-06-25 08:30:51 +0200

    split scheduler code from schedule object code
    Although the scheduler still has some intimate knowledge
    about the current isl_schedule representation, this will
    be much less the case after we switch to a schedule tree
    representation.  Furthermore, the isl_schedule object will
    also become useful to represent schedules not produced by
    the scheduler, so it does not really belong in a file that
    mainly contains the scheduler code.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_schedule_private.h
+++ isl_schedule_private.h
@@ -4,41 +4,6 @@
 #include <isl/aff.h>
 #include <isl/schedule.h>
 
-enum isl_edge_type {
-	isl_edge_validity = 0,
-	isl_edge_first = isl_edge_validity,
-	isl_edge_coincidence,
-	isl_edge_condition,
-	isl_edge_conditional_validity,
-	isl_edge_proximity,
-	isl_edge_last = isl_edge_proximity
-};
-
-/* The constraints that need to be satisfied by a schedule on "domain".
- *
- * "validity" constraints map domain elements i to domain elements
- * that should be scheduled after i.  (Hard constraint)
- * "proximity" constraints map domain elements i to domains elements
- * that should be scheduled as early as possible after i (or before i).
- * (Soft constraint)
- *
- * "condition" and "conditional_validity" constraints map possibly "tagged"
- * domain elements i -> s to "tagged" domain elements j -> t.
- * The elements of the "conditional_validity" constraints, but without the
- * tags (i.e., the elements i -> j) are treated as validity constraints,
- * except that during the construction of a tilable band,
- * the elements of the "conditional_validity" constraints may be violated
- * provided that all adjacent elements of the "condition" constraints
- * are local within the band.
- * A dependence is local within a band if domain and range are mapped
- * to the same schedule point by the band.
- */
-struct isl_schedule_constraints {
-	isl_union_set *domain;
-
-	isl_union_map *constraint[isl_edge_last + 1];
-};
-
 /* The schedule for an individual domain, plus information about the bands
  * and scheduling dimensions.
  * In particular, we keep track of the number of bands and for each
