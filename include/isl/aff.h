06cf557073b0b294cb7a3f101a7cb153673886ae
Sven Verdoolaege <skimo@kotnet.org>
2014-11-25 16:27:49 +0100

    add isl_pw_aff_lt_map
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -294,6 +294,8 @@ __isl_give isl_set *isl_pw_aff_gt_set(__isl_take isl_pw_aff *pwaff1,
 
 __isl_give isl_map *isl_pw_aff_eq_map(__isl_take isl_pw_aff *pa1,
 	__isl_take isl_pw_aff *pa2);
+__isl_give isl_map *isl_pw_aff_lt_map(__isl_take isl_pw_aff *pa1,
+	__isl_take isl_pw_aff *pa2);
 
 __isl_give isl_pw_aff *isl_pw_aff_read_from_str(isl_ctx *ctx, const char *str);
 __isl_give isl_printer *isl_printer_print_pw_aff(__isl_take isl_printer *p,
