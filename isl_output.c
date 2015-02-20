c1e9728901154107ec3ba3ae6f1bcb7de5f63a0b
Sven Verdoolaege <skimo@kotnet.org>
2015-02-20 12:00:07 +0100

    isl_output.c: isl_union_map_print_isl: properly keep track of printer
    Detected by a scan_build on Polly.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_output.c
+++ isl_output.c
@@ -1148,7 +1148,7 @@ static int print_map_body(__isl_take isl_map *map, void *user)
 static __isl_give isl_printer *isl_union_map_print_isl(
 	__isl_keep isl_union_map *umap, __isl_take isl_printer *p)
 {
-	struct isl_union_print_data data = { p, 1 };
+	struct isl_union_print_data data;
 	struct isl_print_space_data space_data = { 0 };
 	isl_space *dim;
 
@@ -1159,6 +1159,8 @@ static __isl_give isl_printer *isl_union_map_print_isl(
 	}
 	isl_space_free(dim);
 	p = isl_printer_print_str(p, s_open_set[0]);
+	data.p = p;
+	data.first = 1;
 	isl_union_map_foreach_map(umap, &print_map_body, &data);
 	p = data.p;
 	p = isl_printer_print_str(p, s_close_set[0]);
