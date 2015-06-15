5978a19fc539313980f47b89a57b3c8e2bdd8d95
Michael Kruse <isl@meinersbur.de>
2015-06-15 14:41:32 +0200

    Backport buildsystem files from devel

---
+++ isl_config.h.cmake
@@ -0,0 +1,14 @@
+
+/* use gmp to implement isl_int */
+#cmakedefine USE_GMP_FOR_MP
+
+/* use imath to implement isl_int */
+#cmakedefine USE_IMATH_FOR_MP
+
+/* use long long to implement isl_int */
+#cmakedefine USE_LONG_FOR_MP
+
+/* optimize for small integers */
+#cmakedefine USE_SMALL_INT_OPT
+
+#include <isl_config_post.h>
