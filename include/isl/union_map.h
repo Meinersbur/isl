b47dbdff19ac3d9fa2500f866038e78282f3e491
Sven Verdoolaege <skimo@kotnet.org>
2015-10-30 17:24:24 +0100

    export isl_union_map_{domain,range}
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/union_map.h
+++ include/isl/union_map.h
@@ -40,7 +40,9 @@ int isl_union_map_find_dim_by_name(__isl_keep isl_union_map *umap,
 __isl_give isl_union_map *isl_union_map_universe(
 	__isl_take isl_union_map *umap);
 __isl_give isl_set *isl_union_map_params(__isl_take isl_union_map *umap);
+__isl_export
 __isl_give isl_union_set *isl_union_map_domain(__isl_take isl_union_map *umap);
+__isl_export
 __isl_give isl_union_set *isl_union_map_range(__isl_take isl_union_map *umap);
 __isl_give isl_union_map *isl_union_map_domain_map(
 	__isl_take isl_union_map *umap);
