922bd39bf1d4a61e1a460243646edcb5b46d44fd
Sven Verdoolaege <skimo@kotnet.org>
2015-10-30 17:26:13 +0100

    export isl_union_map_zip
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/union_map.h
+++ include/isl/union_map.h
@@ -254,6 +254,7 @@ __isl_give isl_union_set *isl_union_map_wrap(__isl_take isl_union_map *umap);
 __isl_export
 __isl_give isl_union_map *isl_union_set_unwrap(__isl_take isl_union_set *uset);
 
+__isl_export
 __isl_give isl_union_map *isl_union_map_zip(__isl_take isl_union_map *umap);
 __isl_give isl_union_map *isl_union_map_curry(__isl_take isl_union_map *umap);
 __isl_give isl_union_map *isl_union_map_range_curry(
