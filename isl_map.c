b806512fa7de04bd17c619728087aa43093e3ca7
Sven Verdoolaege <skimo@kotnet.org>
2015-11-02 17:53:31 +0100

    isl_basic_map_curry: finalize result
    Since isl_basic_map_curry is a public function, its result should be finalized.
    
    Since it does not modify any constraints,
    there is no need to look for any obviously redundant integer divisions.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_map.c
+++ isl_map.c
@@ -11343,6 +11343,7 @@ __isl_give isl_basic_map *isl_basic_map_curry(__isl_take isl_basic_map *bmap)
 	bmap->dim = isl_space_curry(bmap->dim);
 	if (!bmap->dim)
 		goto error;
+	bmap = isl_basic_map_mark_final(bmap);
 	return bmap;
 error:
 	isl_basic_map_free(bmap);
