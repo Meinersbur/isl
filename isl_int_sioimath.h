70eb08ca7d570541ec4ac7c5a4126b11258a1b5a
Michael Kruse <isl@meinersbur.de>
2015-11-30 22:46:14 +0100

    Fix comment formatting
    Signed-off-by: Michael Kruse <isl@meinersbur.de>
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_int_sioimath.h
+++ isl_int_sioimath.h
@@ -522,7 +522,7 @@ inline double isl_sioimath_get_d(isl_sioimath_src val)
 /* Format a number as decimal string.
  *
  * The largest possible string from small representation is 12 characters
- *("-2147483647").
+ * ("-2147483647").
  */
 inline char *isl_sioimath_get_str(isl_sioimath_src val)
 {
@@ -719,7 +719,7 @@ inline void isl_sioimath_mul_si(isl_sioimath_ptr dst, isl_sioimath lhs,
 	isl_sioimath_try_demote(dst);
 }
 
-/* Multiply an isl_int and an unsigned long
+/* Multiply an isl_int and an unsigned long.
  */
 inline void isl_sioimath_mul_ui(isl_sioimath_ptr dst, isl_sioimath lhs,
 	unsigned long rhs)
@@ -739,9 +739,8 @@ inline void isl_sioimath_mul_ui(isl_sioimath_ptr dst, isl_sioimath lhs,
 }
 
 /* Compute the power of an isl_int to an unsigned long.
- * Always let IMath do it; the result is unlikely to be small except some
- * special
- * cases.
+ * Always let IMath do it; the result is unlikely to be small except in some
+ * special cases.
  * Note: 0^0 == 1
  */
 inline void isl_sioimath_pow_ui(isl_sioimath_ptr dst, isl_sioimath_src lhs,
