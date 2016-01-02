Meinersbur/isl

commit daf854625fd18b93a3eabff19d64aee6ecd877e6

add isl_multi_*_scale_multi_val
Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_multi_templ.c
@@ -873,3 +873,39 @@ error:
 	isl_val_free(v);
 	return FN(MULTI(BASE),free)(multi);
 }
+
+/* Multiply the elements of "multi" by the corresponding element of "mv"
+ * and return the result.
+ */
+__isl_give MULTI(BASE) *FN(MULTI(BASE),scale_multi_val)(
+	__isl_take MULTI(BASE) *multi, __isl_take isl_multi_val *mv)
+{
+	int i;
+
+	if (!multi || !mv)
+		goto error;
+
+	if (!isl_space_tuple_match(multi->space, isl_dim_out,
+					mv->space, isl_dim_set))
+		isl_die(isl_multi_val_get_ctx(mv), isl_error_invalid,
+			"spaces don't match", goto error);
+
+	multi = FN(MULTI(BASE),cow)(multi);
+	if (!multi)
+		return NULL;
+
+	for (i = 0; i < multi->n; ++i) {
+		isl_val *v;
+
+		v = isl_multi_val_get_val(mv, i);
+		multi->p[i] = FN(EL,scale_val)(multi->p[i], v);
+		if (!multi->p[i])
+			goto error;
+	}
+
+	isl_multi_val_free(mv);
+	return multi;
+error:
+	isl_multi_val_free(mv);
+	return FN(MULTI(BASE),free)(multi);
+}

doc/user.pod
@@ -3543,6 +3543,9 @@ Operations include
 	__isl_give isl_multi_val *isl_multi_val_scale_val(
 		__isl_take isl_multi_val *mv,
 		__isl_take isl_val *v);
+	__isl_give isl_multi_val *isl_multi_val_scale_multi_val(
+		__isl_take isl_multi_val *mv1,
+		__isl_take isl_multi_val *mv2);
 
 =head2 Vectors
 
@@ -4383,6 +4386,13 @@ C<isl_multi_aff_sub> subtracts the second argument from the first.
 	__isl_give isl_multi_pw_aff *isl_multi_pw_aff_scale_val(
 		__isl_take isl_multi_pw_aff *mpa,
 		__isl_take isl_val *v);
+	__isl_give isl_multi_aff *isl_multi_aff_scale_multi_val(
+		__isl_take isl_multi_aff *ma,
+		__isl_take isl_multi_val *mv);
+	__isl_give isl_multi_pw_aff *
+	isl_multi_pw_aff_scale_multi_val(
+		__isl_take isl_multi_pw_aff *mpa,
+		__isl_take isl_multi_val *mv);
 	__isl_give isl_multi_aff *isl_multi_aff_scale_vec(
 		__isl_take isl_multi_aff *ma,
 		__isl_take isl_vec *v);
@@ -4393,6 +4403,8 @@ C<isl_multi_aff_sub> subtracts the second argument from the first.
 		__isl_take isl_union_pw_multi_aff *upma,
 		__isl_take isl_vec *v);
 
+C<isl_multi_aff_scale_multi_val> scales the elements of C<ma>
+by the corresponding elements of C<mv>.
 C<isl_multi_aff_scale_vec> scales the first elements of C<ma>
 by the corresponding elements of C<v>.
 

include/isl/multi.h
@@ -63,6 +63,9 @@ __isl_give isl_multi_##BASE *isl_multi_##BASE##_range_product(		\
 	__isl_take isl_multi_##BASE *multi2);				\
 __isl_give isl_multi_##BASE *isl_multi_##BASE##_scale_val(		\
 	__isl_take isl_multi_##BASE *multi, __isl_take isl_val *v);	\
+__isl_give isl_multi_##BASE *isl_multi_##BASE##_scale_multi_val(	\
+	__isl_take isl_multi_##BASE *multi,				\
+	__isl_take isl_multi_val *mv);					\
 __isl_give isl_multi_##BASE *isl_multi_##BASE##_align_params(		\
 	__isl_take isl_multi_##BASE *multi,				\
 	__isl_take isl_space *model);

commit 5999388fddae75c397a25d52dcb8519f693e8b9e

isl_multi_*_scale_multi_val: plug memory leak on error path
The leak was introduced in daf8546 (add isl_multi_*_scale_multi_val,
Tue Jun 18 11:28:59 2013 +0200).

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_multi_templ.c
@@ -1205,7 +1205,7 @@ __isl_give MULTI(BASE) *FN(MULTI(BASE),scale_multi_val)(
 
 	multi = FN(MULTI(BASE),cow)(multi);
 	if (!multi)
-		return NULL;
+		goto error;
 
 	for (i = 0; i < multi->n; ++i) {
 		isl_val *v;