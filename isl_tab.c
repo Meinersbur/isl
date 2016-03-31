39be838bfa058b7e1de8e60c94f1779b9a697d59
Sven Verdoolaege <skimo@kotnet.org>
2016-03-31 17:49:51 +0200

    isl_tab_detect_redundant: check later constraints first
    A given set description may have constraints that are mutually redundant.
    That is, either constraint can be removed as redundant, but not both.
    This means that the order in which constraints are considered can
    lead to different results.  The order in which the constraints are
    considered now is difficult to predict by the callers and therefore
    also difficult to control.  Check later constraints first such that
    later constraints will be end up being removed in favor of earlier
    constraints.  This allows the caller to specify which constraints
    are preferred by ordering them.
    
    Note that this new selection mechanism results in some fairly random
    changes in the AST generation outputs exactly because different constraints
    may end up getting preserved.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_tab.c
+++ isl_tab.c
@@ -2990,19 +2990,25 @@ static int con_is_redundant(struct isl_tab *tab, struct isl_tab_var *var)
  * variable can be found.
  * The marked field has only been set for variables that
  * appear in non-redundant rows or non-dead columns.
+ *
+ * Pick the last constraint variable that is marked and
+ * that appears in either a non-redundant row or a non-dead columns.
+ * Since the returned variable is tested for being a redundant constraint,
+ * there is no need to return any tab variable that corresponds to a variable.
  */
 static struct isl_tab_var *select_marked(struct isl_tab *tab)
 {
 	int i;
 	struct isl_tab_var *var;
 
-	for (i = tab->n_redundant; i < tab->n_row; ++i) {
-		var = isl_tab_var_from_row(tab, i);
-		if (var->marked)
-			return var;
-	}
-	for (i = tab->n_dead; i < tab->n_col; ++i) {
-		var = var_from_col(tab, i);
+	for (i = tab->n_con - 1; i >= 0; --i) {
+		var = &tab->con[i];
+		if (var->index < 0)
+			continue;
+		if (var->is_row && var->index < tab->n_redundant)
+			continue;
+		if (!var->is_row && var->index < tab->n_dead)
+			continue;
 		if (var->marked)
 			return var;
 	}
