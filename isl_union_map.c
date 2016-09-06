86248c438807160936dcd3b4ce1109b227b5608d
Sven Verdoolaege <skimo@kotnet.org>
2016-09-06 17:42:49 +0200

    isl_union_map.c: subtract_entry: use isl_map_get_space
    This reduces the dependence on the internal representation.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_union_map.c
+++ isl_union_map.c
@@ -616,11 +616,14 @@ static isl_stat subtract_entry(void **entry, void *user)
 	struct isl_union_map_gen_bin_data *data = user;
 	uint32_t hash;
 	struct isl_hash_table_entry *entry2;
+	isl_space *space;
 	isl_map *map = *entry;
 
-	hash = isl_space_get_hash(map->dim);
+	space = isl_map_get_space(map);
+	hash = isl_space_get_hash(space);
 	entry2 = isl_hash_table_find(data->umap2->dim->ctx, &data->umap2->table,
-				     hash, &has_space, map->dim, 0);
+				     hash, &has_space, space, 0);
+	isl_space_free(space);
 	map = isl_map_copy(map);
 	if (entry2) {
 		int empty;
