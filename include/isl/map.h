bb38638d8b5f60fe451af4eacd137c3000828f0d
Sven Verdoolaege <skimo@kotnet.org>
2016-10-19 14:50:50 +0200

    mark isl_map_copy_basic_map and isl_map_drop_basic_map as deprecated
    These functions are too closely tied to the internal representation and
    will be removed in a future release.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/map.h
+++ include/isl/map.h
@@ -275,7 +275,9 @@ __isl_give isl_pw_multi_aff *isl_map_lexmax_pw_multi_aff(
 void isl_basic_map_print_internal(__isl_keep isl_basic_map *bmap,
 	FILE *out, int indent);
 
+ISL_DEPRECATED
 struct isl_basic_map *isl_map_copy_basic_map(struct isl_map *map);
+ISL_DEPRECATED
 __isl_give isl_map *isl_map_drop_basic_map(__isl_take isl_map *map,
 						__isl_keep isl_basic_map *bmap);
 
