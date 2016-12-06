d388c8fdbf8c7b9ea804b2e8e9793539d3e7fc6e
Sven Verdoolaege <skimo@kotnet.org>
2016-12-06 14:17:17 +0100

    transfer copyright of isl_map_coalesce improvements
    This roughly covers the sequence of commits from 03c7d7a (isl_coalesce.c:
    generalize check_coalesce_subset to coalesce_divs,
    Fri May 27 11:50:49 2016 +0200) up to and including 1f737b4 (isl_coalesce.c:
    fuse: simplify fused result immediately, Sat Jun 11 11:30:43 2016 +0200).
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- isl_coalesce.c
+++ isl_coalesce.c
@@ -3,7 +3,7 @@
  * Copyright 2010      INRIA Saclay
  * Copyright 2012-2013 Ecole Normale Superieure
  * Copyright 2014      INRIA Rocquencourt
- * Copyright 2016      Sven Verdoolaege
+ * Copyright 2016      INRIA Paris
  *
  * Use of this software is governed by the MIT license
  *
@@ -14,6 +14,8 @@
  * and Ecole Normale Superieure, 45 rue d’Ulm, 75230 Paris, France
  * and Inria Paris - Rocquencourt, Domaine de Voluceau - Rocquencourt,
  * B.P. 105 - 78153 Le Chesnay, France
+ * and Centre de Recherche Inria de Paris, 2 rue Simone Iff - Voie DQ12,
+ * CS 42112, 75589 Paris Cedex 12, France
  */
 
 #include <isl_ctx_private.h>
