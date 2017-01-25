0ef2d6845021819446426b4a996848a7952fdb3c
Sven Verdoolaege <skimo@kotnet.org>
2017-01-25 10:04:58 +0100

    mark isl_space_match as deprecated
    This function was never documented and the only sensible use case
    has already been made available as isl_space_has_equal_params.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/space.h
+++ include/isl/space.h
@@ -161,6 +161,7 @@ isl_bool isl_space_has_equal_tuples(__isl_keep isl_space *space1,
 isl_bool isl_space_tuple_is_equal(__isl_keep isl_space *space1,
 	enum isl_dim_type type1, __isl_keep isl_space *space2,
 	enum isl_dim_type type2);
+ISL_DEPRECATED
 isl_bool isl_space_match(__isl_keep isl_space *space1, enum isl_dim_type type1,
 	__isl_keep isl_space *space2, enum isl_dim_type type2);
 ISL_DEPRECATED
