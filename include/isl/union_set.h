954e63a9c8d0ab07863a21d2fe72d887cea1e136
Sven Verdoolaege <skimo@kotnet.org>
2017-02-06 12:00:19 +0100

    isl_union_{map,set}_contains: rename "dim" argument to "space"
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/isl/union_set.h
+++ include/isl/union_set.h
@@ -115,7 +115,7 @@ __isl_export
 isl_stat isl_union_set_foreach_set(__isl_keep isl_union_set *uset,
 	isl_stat (*fn)(__isl_take isl_set *set, void *user), void *user);
 __isl_give int isl_union_set_contains(__isl_keep isl_union_set *uset,
-	__isl_keep isl_space *dim);
+	__isl_keep isl_space *space);
 __isl_give isl_set *isl_union_set_extract_set(__isl_keep isl_union_set *uset,
 	__isl_take isl_space *dim);
 __isl_give isl_set *isl_set_from_union_set(__isl_take isl_union_set *uset);
