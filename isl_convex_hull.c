Meinersbur/isl

commit 298cbeb6e1403ac90b279920a9ceaff7ad184059

convex_hull: coalesce set first

isl_convex_hull.c
@@ -1284,11 +1284,11 @@ static struct isl_basic_set *uset_convex_hull(struct isl_set *set)
 	if (isl_set_n_dim(set) == 0)
 		return convex_hull_0d(set);
 
+	set = isl_set_coalesce(set);
 	set = isl_set_set_rational(set);
 
 	if (!set)
 		goto error;
-	set = isl_set_normalize(set);
 	if (!set)
 		return NULL;
 	if (set->n == 1) {

commit ca2611aedff300bf9ef4d32d2f6399ead0173c4e

isl_convex_hull.c: uset_convex_hull: drop redundant error check
The redundant check was accidentally introduced in 298cbeb (convex_hull:
coalesce set first, Tue Mar 17 14:46:50 2009 +0100).

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

isl_convex_hull.c
@@ -1848,8 +1848,6 @@ static struct isl_basic_set *uset_convex_hull(struct isl_set *set)
 	set = isl_set_set_rational(set);
 
 	if (!set)
-		goto error;
-	if (!set)
 		return NULL;
 	if (set->n == 1) {
 		convex_hull = isl_basic_set_copy(set->p[0]);