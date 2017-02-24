d21989f6e45002c185929d26f7414ecaa9f879cf
Tobias Grosser <tobias@grosser.es>
2017-02-24 08:29:29 +0100

    POLLY-ONLY: Add operator bool()

--- interface/cpp.cc
+++ interface/cpp.cc
@@ -363,6 +363,7 @@ void cpp_generator::print_ptr_decl(ostream &os, const isl_class &clazz)
 	osprintf(os, "  inline __isl_keep %s *get() const;\n", name);
 	osprintf(os, "  inline __isl_give %s *release();\n", name);
 	osprintf(os, "  inline bool is_null() const;\n");
+	osprintf(os, "  inline explicit operator bool() const;\n", name);
 }
 
 /* Print the declaration of the get_ctx method.
@@ -637,6 +638,9 @@ void cpp_generator::print_ptr_impl(ostream &os, const isl_class &clazz)
 	osprintf(os, "bool %s::is_null() const {\n", cppname);
 	osprintf(os, "  return ptr == nullptr;\n");
 	osprintf(os, "}\n");
+	osprintf(os, "%s::operator bool() const {\n", cppname);
+	osprintf(os, "  return !is_null();\n");
+	osprintf(os, "}\n\n");
 }
 
 /* Print the implementation of the get_ctx method.
