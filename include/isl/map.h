df8f57f9b9aed6533caea4d487f642ff894b2b16
Malhar Thakkar <malhar1910@gmail.com>
2017-06-29 23:46:03 +0530

    add missing annotations to declaration of isl_map_union_disjoint
    These annotations prevent some memory leak warnings from being raised while
    performing static analysis.
    
    Signed-off-by: Malhar Thakkar <malhar1910@gmail.com>
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- include/isl/map.h
+++ include/isl/map.h
@@ -301,8 +301,8 @@ __isl_export
 __isl_give isl_map *isl_map_union(
 		__isl_take isl_map *map1,
 		__isl_take isl_map *map2);
-struct isl_map *isl_map_union_disjoint(
-			struct isl_map *map1, struct isl_map *map2);
+__isl_give isl_map *isl_map_union_disjoint(
+		__isl_take isl_map *map1, __isl_take isl_map *map2);
 __isl_export
 __isl_give isl_map *isl_map_intersect_domain(
 		__isl_take isl_map *map,
