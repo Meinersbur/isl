3e2884b431664906af38dabfd42ef79ef682dbc7
Malhar Thakkar <malhar1910@gmail.com>
2017-07-03 18:01:06 +0530

    add missing annotations to isl_basic_set_normalize_constraints
    These annotations prevent some memory leak warnings from being raised while
    performing static analysis.
    
    Signed-off-by: Malhar Thakkar <malhar1910@gmail.com>
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_map_private.h
+++ isl_map_private.h
@@ -298,8 +298,8 @@ isl_bool isl_basic_map_plain_is_equal(__isl_keep isl_basic_map *bmap1,
 	__isl_keep isl_basic_map *bmap2);
 __isl_give isl_basic_map *isl_basic_map_normalize_constraints(
 	__isl_take isl_basic_map *bmap);
-struct isl_basic_set *isl_basic_set_normalize_constraints(
-	struct isl_basic_set *bset);
+__isl_give isl_basic_set *isl_basic_set_normalize_constraints(
+	__isl_take isl_basic_set *bset);
 __isl_give isl_basic_map *isl_basic_map_implicit_equalities(
 	__isl_take isl_basic_map *bmap);
 struct isl_basic_set *isl_basic_map_underlying_set(struct isl_basic_map *bmap);
