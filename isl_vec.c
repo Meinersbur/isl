f89e3ab3f974bd67cd39cf9796619c15016f15d0
Malhar Thakkar <malhar1910@gmail.com>
2017-07-03 18:01:24 +0530

    add missing annotations to isl_vec_ceil
    These annotations prevent some memory leak warnings from being raised while
    performing static analysis.
    
    Signed-off-by: Malhar Thakkar <malhar1910@gmail.com>
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_vec.c
+++ isl_vec.c
@@ -439,7 +439,7 @@ void isl_vec_lcm(struct isl_vec *vec, isl_int *lcm)
 /* Given a rational vector, with the denominator in the first element
  * of the vector, round up all coordinates.
  */
-struct isl_vec *isl_vec_ceil(struct isl_vec *vec)
+__isl_give isl_vec *isl_vec_ceil(__isl_take isl_vec *vec)
 {
 	vec = isl_vec_cow(vec);
 	if (!vec)
