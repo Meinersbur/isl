d1b13635fa966dcf621c0b36e9f1317efb8a0651
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2017-07-28 10:28:53 +0200

    isl_scheduler.c: isl_sched_graph: keep track of original dependence graph
    That is, instead of simply recording whether an isl_sched_graph
    object represents an original dependence graph, record
    the actual original dependence graph from which the graph has
    been split off.  In the isl_sched_graph represents
    an original dependence graph, then it points to itself.
    
    This will be useful for handling node pointers that point
    to the original dependence graph.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_scheduler.c
+++ isl_scheduler.c
@@ -308,8 +308,9 @@ static int is_conditional_validity(struct isl_sched_edge *edge)
  *	rows in the node schedules
  * n_total_row is the current number of rows in the node schedules
  * band_start is the starting row in the node schedules of the current band
- * root is set if this graph is the original dependence graph,
- *	without any splitting
+ * root is set to the the original dependence graph from which this graph
+ *	is derived through splitting.  If this graph is not the result of
+ *	splitting, then the root field points to the graph itself.
  *
  * sorted contains a list of node indices sorted according to the
  *	SCC to which a node belongs
@@ -357,7 +358,7 @@ struct isl_sched_graph {
 	int n_total_row;
 	int band_start;
 
-	int root;
+	struct isl_sched_graph *root;
 
 	struct isl_sched_edge *edge;
 	int n_edge;
@@ -660,7 +661,7 @@ static void graph_free(isl_ctx *ctx, struct isl_sched_graph *graph)
 			isl_map_free(graph->node[i].sched_map);
 			isl_mat_free(graph->node[i].indep);
 			isl_mat_free(graph->node[i].vmap);
-			if (graph->root)
+			if (graph->root == graph)
 				free(graph->node[i].coincident);
 			isl_multi_val_free(graph->node[i].sizes);
 			isl_basic_set_free(graph->node[i].bounds);
@@ -1321,7 +1322,7 @@ static isl_stat graph_init(struct isl_sched_graph *graph,
 
 	if (compute_max_row(graph, sc) < 0)
 		return isl_stat_error;
-	graph->root = 1;
+	graph->root = graph;
 	graph->n = 0;
 	domain = isl_schedule_constraints_get_domain(sc);
 	domain = isl_union_set_intersect_params(domain,
@@ -3502,6 +3503,7 @@ static int extract_sub_graph(isl_ctx *ctx, struct isl_sched_graph *graph,
 			++n_edge;
 	if (graph_alloc(ctx, sub, n, n_edge) < 0)
 		return -1;
+	sub->root = graph->root;
 	if (copy_nodes(sub, graph, node_pred, data) < 0)
 		return -1;
 	if (graph_init_table(ctx, sub) < 0)
