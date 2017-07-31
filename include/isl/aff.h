39ed8d0acb47b1ace461f0effa0447ee9905bc3d
Tobias Grosser <tobias@grosser.es>
2017-07-31 14:00:10 +0200

    cpp/python: export documented functions to construct sets from isl_aff
    The following documented operations are exported:
    
    	isl_aff_eq_set
    	isl_aff_ne_set
    	isl_aff_le_set
    	isl_aff_lt_set
    	isl_aff_ge_set
    	isl_aff_gt_set
    
    The function variants that produce basic sets are not yet exported, as
    it is unclear if there are common use cases for them.
    
    Signed-off-by: Tobias Grosser <tobias@grosser.es>
    Reviewed-by: Nicolas Vasilache <nicolas.vasilache@gmail.com>
    Reviewed-by: Oleksandr Zinenko <oleksandr.zinenko@inria.fr>
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -141,24 +141,30 @@ __isl_give isl_basic_set *isl_aff_neg_basic_set(__isl_take isl_aff *aff);
 
 __isl_give isl_basic_set *isl_aff_eq_basic_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
+__isl_export
 __isl_give isl_set *isl_aff_eq_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
+__isl_export
 __isl_give isl_set *isl_aff_ne_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
 __isl_give isl_basic_set *isl_aff_le_basic_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
+__isl_export
 __isl_give isl_set *isl_aff_le_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
 __isl_give isl_basic_set *isl_aff_lt_basic_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
+__isl_export
 __isl_give isl_set *isl_aff_lt_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
 __isl_give isl_basic_set *isl_aff_ge_basic_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
+__isl_export
 __isl_give isl_set *isl_aff_ge_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
 __isl_give isl_basic_set *isl_aff_gt_basic_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
+__isl_export
 __isl_give isl_set *isl_aff_gt_set(__isl_take isl_aff *aff1,
 	__isl_take isl_aff *aff2);
 
