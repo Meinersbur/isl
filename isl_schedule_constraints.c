3998538b3800570eb44dcd1efea67fb6a8c9a882
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2017-08-18 15:10:03 +0200

    isl_printer_print_schedule_constraints: only print salient fields
    If a field was not explicitly set, then there is no need to print it.
    Similarly, if a field was explicitly set to the default value,
    then there is also no need to print it.
    The domain field is always printed because it does not have
    a default value.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_schedule_constraints.c
+++ isl_schedule_constraints.c
@@ -480,10 +480,21 @@ static char *key_str[] = {
 };
 
 /* Print a key, value pair for the edge of type "type" in "sc" to "p".
+ *
+ * If the edge relation is empty, then it is not printed since
+ * an empty relation is the default value.
  */
 static __isl_give isl_printer *print_constraint(__isl_take isl_printer *p,
 	__isl_keep isl_schedule_constraints *sc, enum isl_edge_type type)
 {
+	isl_bool empty;
+
+	empty = isl_union_map_plain_is_empty(sc->constraint[type]);
+	if (empty < 0)
+		return isl_printer_free(p);
+	if (empty)
+		return p;
+
 	p = isl_printer_print_str(p, key_str[type]);
 	p = isl_printer_yaml_next(p);
 	p = isl_printer_print_union_map(p, sc->constraint[type]);
@@ -495,10 +506,14 @@ static __isl_give isl_printer *print_constraint(__isl_take isl_printer *p,
 /* Print "sc" to "p"
  *
  * In particular, print the isl_schedule_constraints object as a YAML document.
+ * Fields with values that are (obviously) equal to their default values
+ * are not printed.
  */
 __isl_give isl_printer *isl_printer_print_schedule_constraints(
 	__isl_take isl_printer *p, __isl_keep isl_schedule_constraints *sc)
 {
+	isl_bool universe;
+
 	if (!sc)
 		return isl_printer_free(p);
 
@@ -507,10 +522,15 @@ __isl_give isl_printer *isl_printer_print_schedule_constraints(
 	p = isl_printer_yaml_next(p);
 	p = isl_printer_print_union_set(p, sc->domain);
 	p = isl_printer_yaml_next(p);
-	p = isl_printer_print_str(p, key_str[isl_sc_key_context]);
-	p = isl_printer_yaml_next(p);
-	p = isl_printer_print_set(p, sc->context);
-	p = isl_printer_yaml_next(p);
+	universe = isl_set_plain_is_universe(sc->context);
+	if (universe < 0)
+		return isl_printer_free(p);
+	if (!universe) {
+		p = isl_printer_print_str(p, key_str[isl_sc_key_context]);
+		p = isl_printer_yaml_next(p);
+		p = isl_printer_print_set(p, sc->context);
+		p = isl_printer_yaml_next(p);
+	}
 	p = print_constraint(p, sc, isl_edge_validity);
 	p = print_constraint(p, sc, isl_edge_proximity);
 	p = print_constraint(p, sc, isl_edge_coincidence);
