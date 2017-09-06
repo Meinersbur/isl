d1da5488ff9b24397e13550b18aadc7d71350970
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2017-09-06 16:55:42 +0200

    export isl_map_list
    This is needed to be able to export isl_union_map_get_map_list
    in the next commit.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- include/isl/map.h
+++ include/isl/map.h
@@ -775,8 +775,8 @@ __isl_give isl_pw_aff *isl_map_dim_min(__isl_take isl_map *map, int pos);
 __isl_give isl_pw_aff *isl_map_dim_max(__isl_take isl_map *map, int pos);
 
 ISL_DECLARE_LIST_FN(basic_map)
-ISL_DECLARE_LIST_FN(map)
-ISL_DECLARE_LIST_FN_READ(map)
+ISL_DECLARE_EXPORTED_LIST_FN(map)
+ISL_DECLARE_EXPORTED_LIST_FN_READ(map)
 
 #if defined(__cplusplus)
 }
