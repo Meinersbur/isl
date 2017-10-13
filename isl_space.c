73f4f7dc23162c7b986fd56a417d05b769750f5a
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2017-10-13 14:51:55 +0200

    add isl_space_domain_wrapped_domain
    This is a convenience function that will be useful for implementing
    isl_union_map_intersect_domain_wrapped_domain.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_space.c
+++ isl_space.c
@@ -1859,6 +1859,14 @@ __isl_give isl_space *isl_space_factor_range(__isl_take isl_space *space)
 	return space;
 }
 
+/* Given a space of the form [A -> B] -> C, return the space A.
+ */
+__isl_give isl_space *isl_space_domain_wrapped_domain(
+	__isl_take isl_space *space)
+{
+	return isl_space_factor_domain(isl_space_domain(space));
+}
+
 __isl_give isl_space *isl_space_map_from_set(__isl_take isl_space *space)
 {
 	isl_ctx *ctx;
