4cd65f7a8d73d4cd40a4b27c9e30004b8c43e06d
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2017-10-13 14:52:44 +0200

    add isl_space_domain_wrapped_range
    This is a convenience function that will be useful for implementing
    isl_map_intersect_domain_wrapped_domain.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- include/isl/space.h
+++ include/isl/space.h
@@ -121,6 +121,8 @@ __isl_give isl_space *isl_space_range_factor_range(
 	__isl_take isl_space *space);
 __isl_give isl_space *isl_space_domain_wrapped_domain(
 	__isl_take isl_space *space);
+__isl_give isl_space *isl_space_domain_wrapped_range(
+	__isl_take isl_space *space);
 __isl_export
 __isl_give isl_space *isl_space_map_from_set(__isl_take isl_space *space);
 __isl_give isl_space *isl_space_map_from_domain_and_range(
--- isl_space.c
+++ isl_space.c
@@ -1867,6 +1867,14 @@ __isl_give isl_space *isl_space_domain_wrapped_domain(
 	return isl_space_factor_domain(isl_space_domain(space));
 }
 
+/* Given a space of the form [A -> B] -> C, return the space B.
+ */
+__isl_give isl_space *isl_space_domain_wrapped_range(
+	__isl_take isl_space *space)
+{
+	return isl_space_factor_range(isl_space_domain(space));
+}
+
 __isl_give isl_space *isl_space_map_from_set(__isl_take isl_space *space)
 {
 	isl_ctx *ctx;
