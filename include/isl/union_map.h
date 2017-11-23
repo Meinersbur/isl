e3fb1bc42a31b8966c1702fcc54394812ebdcf13
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2017-11-23 15:55:44 +0100

    export isl_union_map_from_{domain,range}
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- include/isl/union_map.h
+++ include/isl/union_map.h
@@ -55,8 +55,10 @@ __isl_give isl_union_map *isl_union_map_range_map(
 	__isl_take isl_union_map *umap);
 __isl_give isl_union_map *isl_union_set_wrapped_domain_map(
 	__isl_take isl_union_set *uset);
+__isl_export
 __isl_give isl_union_map *isl_union_map_from_domain(
 	__isl_take isl_union_set *uset);
+__isl_export
 __isl_give isl_union_map *isl_union_map_from_range(
 	__isl_take isl_union_set *uset);
 
