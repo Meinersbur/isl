1d76c4a90f0525efb646d4f6f0af377f5046a3c8
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2017-11-28 12:50:55 +0100

    cpp: print body of copy constructor on new lines
    This will make it easier to insert error handling code
    in the unchecked C++ bindings.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- interface/cpp.cc
+++ interface/cpp.cc
@@ -480,8 +480,11 @@ void cpp_generator::print_public_constructors_impl(ostream &os,
 	const char *cppname = cppstring.c_str();
 
 	osprintf(os, "%s::%s()\n    : ptr(nullptr) {}\n\n", cppname, cppname);
-	osprintf(os, "%s::%s(const isl::%s &obj)\n    : ptr(obj.copy()) {}\n",
+	osprintf(os, "%s::%s(const isl::%s &obj)\n    : ptr(nullptr)\n",
 		 cppname, cppname, cppname);
+	osprintf(os, "{\n");
+	osprintf(os, "  ptr = obj.copy();\n");
+	osprintf(os, "}\n");
 }
 
 /* Print implementations of constructors for class "clazz" to "os".
