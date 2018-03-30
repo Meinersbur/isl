fe6c8dfa3ca0b431aa29e0d6e1c7d7a1877ec55d
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-03-30 13:45:14 +0200

    add isl_ast_expr_op_get_arg as an alternative name for isl_ast_expr_get_op_arg
    The new name is more consistent with the pattern of other function names,
    where the (sub)type name appears as a prefix.
    The new name will also make it easier to export isl_ast_expr
    as subclasses.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_ast.c
+++ isl_ast.c
@@ -311,7 +311,9 @@ isl_size isl_ast_expr_get_op_n_arg(__isl_keep isl_ast_expr *expr)
 	return isl_ast_expr_op_get_n_arg(expr);
 }
 
-__isl_give isl_ast_expr *isl_ast_expr_get_op_arg(__isl_keep isl_ast_expr *expr,
+/* Return the argument at position "pos" of the operation represented by "expr".
+ */
+__isl_give isl_ast_expr *isl_ast_expr_op_get_arg(__isl_keep isl_ast_expr *expr,
 	int pos)
 {
 	if (!expr)
@@ -324,6 +326,14 @@ __isl_give isl_ast_expr *isl_ast_expr_get_op_arg(__isl_keep isl_ast_expr *expr,
 			"index out of bounds", return NULL);
 
 	return isl_ast_expr_copy(expr->u.op.args[pos]);
+}
+
+/* This is an alternative name for the function above.
+ */
+__isl_give isl_ast_expr *isl_ast_expr_get_op_arg(__isl_keep isl_ast_expr *expr,
+	int pos)
+{
+	return isl_ast_expr_op_get_arg(expr, pos);
 }
 
 /* Replace the argument at position "pos" of "expr" by "arg".
