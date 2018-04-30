55c2be1344867d9e83cbc394b8f041603794473d
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-04-30 14:32:14 +0200

    isl_multi_templ.c: extract out isl_multi_from_base_templ.c
    This makes the function isl_multi_*_from_* opt-in rather than opt-out.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_aff.c
+++ isl_aff.c
@@ -3820,6 +3820,7 @@ error:
 #include <isl_multi_cmp.c>
 #include <isl_multi_dims.c>
 #include <isl_multi_floor.c>
+#include <isl_multi_from_base_templ.c>
 #include <isl_multi_gist.c>
 #include <isl_multi_identity_templ.c>
 
@@ -6164,6 +6165,7 @@ error:
 #include <isl_multi_coalesce.c>
 #include <isl_multi_domain_templ.c>
 #include <isl_multi_dims.c>
+#include <isl_multi_from_base_templ.c>
 #include <isl_multi_gist.c>
 #include <isl_multi_hash.c>
 #include <isl_multi_identity_templ.c>
@@ -7920,6 +7922,7 @@ error:
 #include <isl_multi_apply_union_set.c>
 #include <isl_multi_coalesce.c>
 #include <isl_multi_floor.c>
+#include <isl_multi_from_base_templ.c>
 #include <isl_multi_gist.c>
 #include <isl_multi_align_set.c>
 #include <isl_multi_align_union_set.c>
--- isl_multi_templ.c
+++ isl_multi_templ.c
@@ -630,33 +630,6 @@ __isl_give MULTI(BASE) *FN(MULTI(BASE),zero)(__isl_take isl_space *space)
 }
 #endif
 
-#ifndef NO_FROM_BASE
-/* Create a multiple expression with a single output/set dimension
- * equal to "el".
- * For most multiple expression types, the base type has a single
- * output/set dimension and the space of the result is therefore
- * the same as the space of the input.
- * In the case of isl_multi_union_pw_aff, however, the base type
- * lives in a parameter space and we therefore need to add
- * a single set dimension.
- */
-__isl_give MULTI(BASE) *FN(FN(MULTI(BASE),from),BASE)(__isl_take EL *el)
-{
-	isl_space *space;
-	MULTI(BASE) *multi;
-
-	space = FN(EL,get_space(el));
-	if (isl_space_is_params(space)) {
-		space = isl_space_set_from_params(space);
-		space = isl_space_add_dims(space, isl_dim_set, 1);
-	}
-	multi = FN(MULTI(BASE),alloc)(space);
-	multi = FN(FN(MULTI(BASE),set),BASE)(multi, 0, el);
-
-	return multi;
-}
-#endif
-
 __isl_give MULTI(BASE) *FN(MULTI(BASE),drop_dims)(
 	__isl_take MULTI(BASE) *multi,
 	enum isl_dim_type type, unsigned first, unsigned n)
