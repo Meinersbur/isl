47ba1c82c33226cebce5bb50eb2e15432340ff52
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-07-01 19:31:28 +0200

    move generated (checked) C++ bindings to include/isl/cpp-checked.h
    The unchecked C++ bindings that will be introduced in an upcoming
    commit will be generated as "include/isl/cpp.h".
    This commit moves the checked C++ bindings to the same directory.
    
    Requested-by: Tobias Grosser <tobias.grosser@inf.ethz.ch>
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_test_cpp-checked.cc
+++ isl_test_cpp-checked.cc
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 
 #include <isl/options.h>
-#include "interface/cpp-checked.h"
+#include <isl/cpp-checked.h>
 
 static void assert_impl(bool condition, const char *file, int line,
 	const char *message)
