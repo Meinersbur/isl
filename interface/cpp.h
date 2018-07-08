76f04393c404d65ee25c213ab3cf89fe5e5a4dbf
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-07-08 16:54:44 +0200

    make cpp_generator::type2cpp(string) public (and static)
    It is not clear why this method, introduced in isl-0.18-598-g81c38ef4c
    (cpp: generate C++ wrapper classes, Sat Apr 8 05:12:50 2017 +0200),
    wasn't made static in the first place.
    Turn it into a public method such that it can be reused
    by the generator for conversions between the default and
    the checked C++ bindings.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- interface/cpp.h
+++ interface/cpp.h
@@ -87,10 +87,11 @@ private:
 	void print_callback_local(ostream &os, ParmVarDecl *param);
 	std::string rename_method(std::string name);
 	string type2cpp(const isl_class &clazz);
-	string type2cpp(string type_string);
 	string type2cpp(QualType type);
 	bool is_implicit_conversion(const isl_class &clazz, FunctionDecl *cons);
 	bool is_subclass(QualType subclass_type, const isl_class &class_type);
 	function_kind get_method_kind(const isl_class &clazz,
 		FunctionDecl *method);
+public:
+	static string type2cpp(string type_string);
 };
