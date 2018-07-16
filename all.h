607b8a9bf83dbc1db096e038a503fa8688641ac7
Tobias Grosser <tobias@grosser.es>
2018-07-16 19:49:08 +0200

    Export more files

--- all.h
+++ all.h
@@ -1,7 +1,10 @@
 #include <isl/val.h>
 #include <isl/aff.h>
 #include <isl/set.h>
+#include <isl/space.h>
+#include <isl/id.h>
 #include <isl/map.h>
+#include <isl/vec.h>
 #include <isl/ilp.h>
 #include <isl/union_set.h>
 #include <isl/union_map.h>
@@ -9,3 +12,6 @@
 #include <isl/schedule.h>
 #include <isl/schedule_node.h>
 #include <isl/ast_build.h>
+#include <isl/constraint.h>
+#include <isl/polynomial.h>
+#include <isl/mat.h>
