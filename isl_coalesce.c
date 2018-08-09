7d6a9a9cc0cb3c5c057f45d65847d2d6709fde70
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-08-09 10:53:51 +0200

    isl_coalesce.c: add_subs: return isl_stat
    This clarifies what the possible return values are.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_coalesce.c
+++ isl_coalesce.c
@@ -3461,7 +3461,7 @@ static isl_stat add_sub_vars(struct isl_coalesce_info *info,
  * This function assumes that a sufficient number of rows and
  * elements in the constraint array are available in the tableau.
  */
-static int add_sub_equalities(struct isl_tab *tab,
+static isl_stat add_sub_equalities(struct isl_tab *tab,
 	__isl_keep isl_aff_list *list, int dim)
 {
 	int i, n;
@@ -3474,7 +3474,7 @@ static int add_sub_equalities(struct isl_tab *tab,
 	ctx = isl_tab_get_ctx(tab);
 	sub = isl_vec_alloc(ctx, 1 + dim + n);
 	if (!sub)
-		return -1;
+		return isl_stat_error;
 	isl_seq_clr(sub->el + 1 + dim, n);
 
 	for (i = 0; i < n; ++i) {
@@ -3494,11 +3494,11 @@ static int add_sub_equalities(struct isl_tab *tab,
 	}
 
 	isl_vec_free(sub);
-	return 0;
+	return isl_stat_ok;
 error:
 	isl_aff_free(aff);
 	isl_vec_free(sub);
-	return -1;
+	return isl_stat_error;
 }
 
 /* Add variables to info->tab and info->bmap corresponding to the elements
@@ -3509,24 +3509,24 @@ error:
  * When this function returns, the total number of variables in info->tab
  * is equal to "dim" plus the number of elements in "list".
  */
-static int add_subs(struct isl_coalesce_info *info,
+static isl_stat add_subs(struct isl_coalesce_info *info,
 	__isl_keep isl_aff_list *list, int dim)
 {
 	int extra_var;
 	int n;
 
 	if (!list)
-		return -1;
+		return isl_stat_error;
 
 	n = isl_aff_list_n_aff(list);
 	extra_var = n - (info->tab->n_var - dim);
 
 	if (isl_tab_extend_vars(info->tab, extra_var) < 0)
-		return -1;
+		return isl_stat_error;
 	if (isl_tab_extend_cons(info->tab, 2 * extra_var) < 0)
-		return -1;
+		return isl_stat_error;
 	if (add_sub_vars(info, list, dim, extra_var) < 0)
-		return -1;
+		return isl_stat_error;
 
 	return add_sub_equalities(info->tab, list, dim);
 }
