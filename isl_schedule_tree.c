6c7810239aa07c49d7f1f885ce663c9c88e681ad
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2018-08-17 14:35:18 +0200

    isl_schedule_tree.c: any_coincident: improve error handling
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_schedule_tree.c
+++ isl_schedule_tree.c
@@ -2618,16 +2618,20 @@ error:
 
 /* Are any members in "band" marked coincident?
  */
-static int any_coincident(__isl_keep isl_schedule_band *band)
+static isl_bool any_coincident(__isl_keep isl_schedule_band *band)
 {
 	int i, n;
 
 	n = isl_schedule_band_n_member(band);
-	for (i = 0; i < n; ++i)
-		if (isl_schedule_band_member_get_coincident(band, i))
-			return 1;
+	for (i = 0; i < n; ++i) {
+		isl_bool coincident;
 
-	return 0;
+		coincident = isl_schedule_band_member_get_coincident(band, i);
+		if (coincident < 0 || coincident)
+			return coincident;
+	}
+
+	return isl_bool_false;
 }
 
 /* Print the band node "band" to "p".
@@ -2641,6 +2645,7 @@ static __isl_give isl_printer *print_tree_band(__isl_take isl_printer *p,
 {
 	isl_union_set *options;
 	int empty;
+	isl_bool coincident;
 
 	p = isl_printer_print_str(p, "schedule");
 	p = isl_printer_yaml_next(p);
@@ -2653,7 +2658,10 @@ static __isl_give isl_printer *print_tree_band(__isl_take isl_printer *p,
 		p = isl_printer_yaml_next(p);
 		p = isl_printer_print_int(p, 1);
 	}
-	if (any_coincident(band)) {
+	coincident = any_coincident(band);
+	if (coincident < 0)
+		return isl_printer_free(p);
+	if (coincident) {
 		int i, n;
 		int style;
 
