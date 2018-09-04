132e18c52196b7392d4ba5d4cafa937f1cab2c19
Tobias Grosser <tobias@grosser.es>
2018-09-04 09:00:40 +0200

    isl_val_is_negone: use isl_bool_ok
    Signed-off-by: Tobias Grosser <tobias@grosser.es>
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_val.c
+++ isl_val.c
@@ -1215,7 +1215,7 @@ isl_bool isl_val_is_negone(__isl_keep isl_val *v)
 	if (!v)
 		return isl_bool_error;
 
-	return isl_int_is_neg(v->n) && isl_int_abs_eq(v->n, v->d);
+	return isl_bool_ok(isl_int_is_neg(v->n) && isl_int_abs_eq(v->n, v->d));
 }
 
 /* Is "v" (strictly) positive?
