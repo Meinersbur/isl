704f6537ba6a72ccce84c30e13deda3aa5ba417a
Tobias Grosser <tobias@grosser.es>
2018-09-04 09:00:35 +0200

    isl_val_is_nan: use isl_bool_ok
    Signed-off-by: Tobias Grosser <tobias@grosser.es>
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_val.c
+++ isl_val.c
@@ -1162,7 +1162,7 @@ isl_bool isl_val_is_nan(__isl_keep isl_val *v)
 	if (!v)
 		return isl_bool_error;
 
-	return isl_int_is_zero(v->n) && isl_int_is_zero(v->d);
+	return isl_bool_ok(isl_int_is_zero(v->n) && isl_int_is_zero(v->d));
 }
 
 /* Does "v" represent +infinity?
