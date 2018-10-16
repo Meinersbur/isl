8c042d34321126d632e64187cddc72d2c33fe036
Sven Verdoolaege <sven@cerebras.net>
2018-10-16 10:39:10 +0200

    export isl_multi_*_zero
    Now that isl_space has been exported, it becomes possible
    to construct a zero-valued function from a space.
    Since the space argument does not uniquely determine
    which function should be constructed, this function
    needs to be exported as a named constructor.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- include/isl/multi.h
+++ include/isl/multi.h
@@ -25,6 +25,7 @@ int isl_multi_##BASE##_find_dim_by_name(				\
 	enum isl_dim_type type, const char *name);			\
 __isl_give isl_multi_##BASE *isl_multi_##BASE##_from_##BASE##_list(	\
 	__isl_take isl_space *space, __isl_take isl_##BASE##_list *list); \
+__isl_export								\
 __isl_give isl_multi_##BASE *isl_multi_##BASE##_zero(			\
 	__isl_take isl_space *space);					\
 __isl_give isl_multi_##BASE *isl_multi_##BASE##_copy(			\
