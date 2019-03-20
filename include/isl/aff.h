863273f2271891c41f433411e885ac6ea150f285
Manjunath Kudlur <manjunath@cerebras.net>
2019-03-20 13:39:24 -0700

    add exported isl_pw_multi_aff_set_range_tuple_id
    In analogy with isl_pw_multi_aff_get_range_tuple_id.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -658,6 +658,9 @@ isl_bool isl_pw_multi_aff_has_tuple_id(__isl_keep isl_pw_multi_aff *pma,
 __isl_give isl_pw_multi_aff *isl_pw_multi_aff_set_tuple_id(
 	__isl_take isl_pw_multi_aff *pma,
 	enum isl_dim_type type, __isl_take isl_id *id);
+__isl_overload
+__isl_give isl_pw_multi_aff *isl_pw_multi_aff_set_range_tuple_id(
+	__isl_take isl_pw_multi_aff *pma, __isl_take isl_id *id);
 __isl_give isl_pw_multi_aff *isl_pw_multi_aff_reset_tuple_id(
 	__isl_take isl_pw_multi_aff *pma, enum isl_dim_type type);
 __isl_give isl_pw_multi_aff *isl_pw_multi_aff_reset_user(
