f3673514c74b2123c02543d7e4fd9ab1892746b3
Sven Verdoolaege <sven@cerebras.net>
2019-04-16 17:37:37 +0200

    add exported isl_union_pw_multi_aff_plain_is_empty
    This is similar to isl_union_map_plain_is_empty.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -859,6 +859,9 @@ __isl_export
 __isl_give isl_pw_multi_aff *isl_union_pw_multi_aff_as_pw_multi_aff(
 	__isl_take isl_union_pw_multi_aff *upma);
 
+__isl_export
+isl_bool isl_union_pw_multi_aff_plain_is_empty(
+	__isl_keep isl_union_pw_multi_aff *upma);
 isl_bool isl_union_pw_multi_aff_involves_nan(
 	__isl_keep isl_union_pw_multi_aff *upma);
 isl_bool isl_union_pw_multi_aff_plain_is_equal(
