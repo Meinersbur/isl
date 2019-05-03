c1004a1491e6c8748eccfd24400e2bb99dac28b5
Sven Verdoolaege <sven@cerebras.net>
2019-05-03 18:23:17 +0200

    export isl_map_{domain,range}_product
    This complements the already exported isl_union_map_{domain,range}_product.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- include/isl/map.h
+++ include/isl/map.h
@@ -333,8 +333,10 @@ __isl_give isl_basic_map *isl_basic_map_domain_product(
 	__isl_take isl_basic_map *bmap1, __isl_take isl_basic_map *bmap2);
 __isl_give isl_basic_map *isl_basic_map_range_product(
 	__isl_take isl_basic_map *bmap1, __isl_take isl_basic_map *bmap2);
+__isl_export
 __isl_give isl_map *isl_map_domain_product(__isl_take isl_map *map1,
 	__isl_take isl_map *map2);
+__isl_export
 __isl_give isl_map *isl_map_range_product(__isl_take isl_map *map1,
 	__isl_take isl_map *map2);
 __isl_give isl_basic_map *isl_basic_map_flat_product(
