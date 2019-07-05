41c14f4b1761e36afa3e6d835f3bb68ae28e99a3
Sven Verdoolaege <sven@cerebras.net>
2019-07-05 14:51:04 +0200

    isl_union_map.c: un_op: allow missing transformation function
    This allows un_op to be used in a mode
    that only filters some of the maps.
    This will be used to implement isl_union_map_intersect_range_space
    in an upcoming commit.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- isl_union_map.c
+++ isl_union_map.c
@@ -1792,7 +1792,7 @@ __isl_give isl_union_map *isl_union_map_flat_range_product(
  * are taken into account.  "filter_user" is passed as the second argument
  * to "filter".  No filter can be set if "inplace" or
  * "total" is set.
- * Exactly one of "fn_map" or "fn_map2" should be set, specifying
+ * At most one of "fn_map" or "fn_map2" can be set, specifying
  * how the maps (selected by "filter") should be transformed.
  * If "fn_map2" is set, then "fn_map2_user" is passed as the second argument.
  */
@@ -1837,7 +1837,7 @@ struct isl_union_map_un_data {
  *
  * If control->filter is set, then check if this map satisfies the filter.
  * If so (or if control->filter is not set), modify the map
- * by calling control->fn_map or control->fn_map2 and
+ * by calling control->fn_map or control->fn_map2 (if set) and
  * either add the result to data->res or
  * replace the original entry by the result (if control->inplace is set).
  */
@@ -1860,7 +1860,7 @@ static isl_stat un_entry(void **entry, void *user)
 	map = isl_map_copy(map);
 	if (control->fn_map2 != NULL)
 		map = control->fn_map2(map, control->fn_map2_user);
-	else
+	else if (control->fn_map != NULL)
 		map = control->fn_map(map);
 	if (!map)
 		return isl_stat_error;
@@ -1890,9 +1890,9 @@ static __isl_give isl_union_map *un_op(__isl_take isl_union_map *umap,
 
 	if (!umap)
 		return NULL;
-	if (!!control->fn_map == !!control->fn_map2)
+	if (!!control->fn_map && !!control->fn_map2)
 		isl_die(isl_union_map_get_ctx(umap), isl_error_internal,
-			"exactly one mapping function should be specified",
+			"at most one mapping function can be specified",
 			return isl_union_map_free(umap));
 	if ((control->inplace || control->total) && control->filter)
 		isl_die(isl_union_map_get_ctx(umap), isl_error_invalid,
