ba2d9923b25130dcf0e2e1f9c69c002a8f17f34d
Sven Verdoolaege <sven@cerebras.net>
2019-07-30 16:27:54 +0200

    isl_basic_map_flatten: directly mark result as final
    Since this function does not modify any constraints,
    there is no need to look for any obviously redundant integer divisions.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- isl_map.c
+++ isl_map.c
@@ -11612,7 +11612,7 @@ __isl_give isl_basic_map *isl_basic_map_flatten(__isl_take isl_basic_map *bmap)
 	if (!bmap->dim)
 		goto error;
 
-	bmap = isl_basic_map_finalize(bmap);
+	bmap = isl_basic_map_mark_final(bmap);
 
 	return bmap;
 error:
