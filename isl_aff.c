cd464e50bbb92ded111883013f55cd9bb1537291
Sven Verdoolaege <sven@cerebras.net>
2019-09-10 10:51:59 +0200

    add isl_aff_nan_on_domain_space
    This provides an alternative to isl_aff_nan_on_domain
    that does not require the user to explicitly construct
    an isl_local_space.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- isl_aff.c
+++ isl_aff.c
@@ -196,6 +196,14 @@ __isl_give isl_aff *isl_aff_nan_on_domain(__isl_take isl_local_space *ls)
 	return isl_aff_set_nan(aff);
 }
 
+/* Return an affine expression defined on the specified domain space
+ * that represents NaN.
+ */
+__isl_give isl_aff *isl_aff_nan_on_domain_space(__isl_take isl_space *space)
+{
+	return isl_aff_nan_on_domain(isl_local_space_from_space(space));
+}
+
 /* Return a piecewise affine expression defined on the specified domain
  * that represents NaN.
  */
