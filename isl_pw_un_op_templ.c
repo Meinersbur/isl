344964ea45193a45aca587be878bdac8fffae492
Sven Verdoolaege <sven@cerebras.net>
2019-10-24 12:21:56 +0200

    isl_pw_*_un_op: allow domain modification
    This will be needed in the implementation of isl_pw_*_domain_reverse.
    
    The order in which the base expressions are considered needs
    to be reversed because the call to
    isl_pw_*_exploit_equalities_and_remove_if_empty may end up
    removing the current cell (replacing it with the last cell).
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- isl_pw_un_op_templ.c
+++ isl_pw_un_op_templ.c
@@ -13,15 +13,21 @@
 /* Data structure that specifies how isl_pw_*_un_op should
  * modify its input.
  *
+ * If "fn_domain" is set, then it is applied to the cells.
+ *
  * "fn_base" is applied to each base expression.
  * This function is assumed to have no effect on the default value
  * (i.e., zero for those objects with a default value).
  */
 S(PW,un_op_control) {
+	__isl_give isl_set *(*fn_domain)(__isl_take isl_set *domain);
 	__isl_give EL *(*fn_base)(__isl_take EL *el);
 };
 
 /* Modify "pw" based on "control".
+ *
+ * If the cells are modified, then the corresponding base expressions
+ * may need to be adjusted to the possibly modified equality constraints.
  */
 static __isl_give PW *FN(PW,un_op)(__isl_take PW *pw,
 	S(PW,un_op_control) *control)
@@ -33,12 +39,22 @@ static __isl_give PW *FN(PW,un_op)(__isl_take PW *pw,
 	if (n < 0)
 		return FN(PW,free)(pw);
 
-	for (i = 0; i < n; ++i) {
+	for (i = n - 1; i >= 0; --i) {
 		EL *el;
+		isl_set *domain;
 
 		el = FN(PW,take_base_at)(pw, i);
 		el = control->fn_base(el);
 		pw = FN(PW,restore_base_at)(pw, i, el);
+
+		if (!control->fn_domain)
+			continue;
+
+		domain = FN(PW,take_domain_at)(pw, i);
+		domain = control->fn_domain(domain);
+		pw = FN(PW,restore_domain_at)(pw, i, domain);
+
+		pw = FN(PW,exploit_equalities_and_remove_if_empty)(pw, i);
 	}
 
 	return pw;
