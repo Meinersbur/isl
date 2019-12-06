860bb85b1f6be976cb5f65b580b81283aa060174
Sven Verdoolaege <sven@cerebras.net>
2019-12-06 15:40:54 +0100

    isl_map_intersect_params: fix argument type
    Since isl_set is equal to isl_map internally, the original code
    was harmless, but still confusing.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- isl_map.c
+++ isl_map.c
@@ -3826,9 +3826,10 @@ static __isl_give isl_map *map_intersect_params(__isl_take isl_map *map,
 }
 
 __isl_give isl_map *isl_map_intersect_params(__isl_take isl_map *map1,
-	__isl_take isl_map *map2)
+	__isl_take isl_set *params)
 {
-	return isl_map_align_params_map_map_and(map1, map2, &map_intersect_params);
+	return isl_map_align_params_map_map_and(map1, params,
+						&map_intersect_params);
 }
 
 __isl_give isl_set *isl_set_intersect_params(__isl_take isl_set *set,
