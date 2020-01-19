e9264c6621c67eef8bd9476cb8b23b02fb48f63e
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2020-01-19 08:56:23 +0100

    isl_basic_map_from_constraint_matrices: rename "dim" argument to "space"
    This was missing from isl-0.20-134-g063beb56df
    (isl_basic_map_from_constraint_matrices: rename "dim" argument to "space",
    Mon Aug 8 14:00:00 2016 +0200).
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- include/isl/map.h
+++ include/isl/map.h
@@ -717,7 +717,7 @@ __isl_give isl_mat *isl_basic_map_inequalities_matrix(
 		enum isl_dim_type c2, enum isl_dim_type c3,
 		enum isl_dim_type c4, enum isl_dim_type c5);
 __isl_give isl_basic_map *isl_basic_map_from_constraint_matrices(
-	__isl_take isl_space *dim,
+	__isl_take isl_space *space,
 	__isl_take isl_mat *eq, __isl_take isl_mat *ineq, enum isl_dim_type c1,
 	enum isl_dim_type c2, enum isl_dim_type c3,
 	enum isl_dim_type c4, enum isl_dim_type c5);
