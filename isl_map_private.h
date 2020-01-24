35bbc98b0c02c50b688c12777dac2509b561ab32
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2020-01-24 10:57:30 +0100

    isl_basic_map_reset_space: rename "dim" argument to "space"
    This was missing from isl-0.15-88-g399fb97e79 (isl_basic_map_reset_space:
    rename "dim" variable to "space", Thu Jul 9 13:21:48 2015 +0200).
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_map_private.h
+++ isl_map_private.h
@@ -169,7 +169,7 @@ __isl_keep isl_space *isl_set_peek_space(__isl_keep isl_set *set);
 __isl_give isl_basic_set *isl_basic_set_reset_space(
 	__isl_take isl_basic_set *bset, __isl_take isl_space *dim);
 __isl_give isl_basic_map *isl_basic_map_reset_space(
-	__isl_take isl_basic_map *bmap, __isl_take isl_space *dim);
+	__isl_take isl_basic_map *bmap, __isl_take isl_space *space);
 __isl_give isl_map *isl_map_reset_space(__isl_take isl_map *map,
 	__isl_take isl_space *space);
 __isl_give isl_map *isl_map_reset_equal_dim_space(__isl_take isl_map *map,
