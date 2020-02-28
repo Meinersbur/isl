5dc0f0185ad1e7bb285bed5c648e0eeb109098a8
Sven Verdoolaege <sven@cerebras.net>
2020-02-28 14:00:39 +0100

    isl_pw_*_union_opt_cmp: keep cells together with piecewise expression
    This will make it easier to swap the two pairs in an upcoming commit.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- isl_pw_union_opt.c
+++ isl_pw_union_opt.c
@@ -69,29 +69,52 @@ static __isl_give isl_set *FN(PW,worse_or_out)(__isl_take isl_set *set,
 	return isl_set_union(set_worse, set_out);
 }
 
-/* Given two piecewise expressions "pw1" and "pw2", replace their domains
- * by the sets in "list1" and "list2" and combine the results into
+/* Internal data structure used by isl_pw_*_union_opt_cmp
+ * that keeps track of a piecewise expression with updated cells.
+ * "pw" holds the original piecewise expression.
+ * "list" holds the updated cells.
+ */
+S(PW,union_opt_cmp_data) {
+	PW *pw;
+	isl_set_list *cell;
+};
+
+/* Free all memory allocated for "data".
+ */
+static void FN(PW,union_opt_cmp_data_clear)(S(PW,union_opt_cmp_data) *data)
+{
+	isl_set_list_free(data->cell);
+	FN(PW,free)(data->pw);
+}
+
+/* Given two piecewise expressions data1->pw and data2->pw, replace
+ * their domains
+ * by the sets in data1->cell and data2->cell and combine the results into
  * a single piecewise expression.
- * The pieces of "pw1" and "pw2" are assumed to have been sorted
+ * The pieces of data1->pw and data2->pw are assumed to have been sorted
  * according to the function value expressions.
  * The pieces of the result are also sorted in this way.
  *
- * Run through the pieces of "pw1" and "pw2" in order until they
- * have both been exhausted, picking the piece from "pw1" or "pw2"
+ * Run through the pieces of data1->pw and data2->pw in order until they
+ * have both been exhausted, picking the piece from data1->pw or data2->pw
  * depending on which should come first, together with the corresponding
- * domain from "list1" or "list2".  In cases where the next pieces
- * in both "pw1" and "pw2" have the same function value expression,
+ * domain from data1->cell or data2->cell.  In cases where the next pieces
+ * in both data1->pw and data2->pw have the same function value expression,
  * construct only a single piece in the result with as domain
- * the union of the domains in "list1" and "list2".
+ * the union of the domains in data1->cell and data2->cell.
  */
-static __isl_give PW *FN(PW,merge)(__isl_take PW *pw1, __isl_take PW *pw2,
-	__isl_take isl_set_list *list1, __isl_take isl_set_list *list2)
+static __isl_give PW *FN(PW,merge)(S(PW,union_opt_cmp_data) *data1,
+	S(PW,union_opt_cmp_data) *data2)
 {
 	int i, j;
 	PW *res;
+	PW *pw1 = data1->pw;
+	PW *pw2 = data2->pw;
+	isl_set_list *list1 = data1->cell;
+	isl_set_list *list2 = data2->cell;
 
 	if (!pw1 || !pw2)
-		goto error;
+		return NULL;
 
 	res = FN(PW,alloc_size)(isl_space_copy(pw1->dim), pw1->n + pw2->n);
 
@@ -125,17 +148,7 @@ static __isl_give PW *FN(PW,merge)(__isl_take PW *pw1, __isl_take PW *pw2,
 		res = FN(PW,add_piece)(res, set, el);
 	}
 
-	isl_set_list_free(list1);
-	isl_set_list_free(list2);
-	FN(PW,free)(pw1);
-	FN(PW,free)(pw2);
 	return res;
-error:
-	isl_set_list_free(list1);
-	isl_set_list_free(list2);
-	FN(PW,free)(pw1);
-	FN(PW,free)(pw2);
-	return NULL;
 }
 
 /* Given a function "cmp" that returns the set of elements where
@@ -164,7 +177,7 @@ error:
  *
  * Since all pairs of pieces are considered, the domains are updated
  * several times.  A and B refer to these updated domains
- * (kept track of in "list1" and "list2"), while A' and B' refer
+ * (kept track of in data[0].cell and data[1].cell), while A' and B' refer
  * to the original domains of the pieces.  It is safe to use these
  * original domains because the difference between, say, A' and A is
  * the domains of pw2-pieces that have been removed before and
@@ -173,7 +186,7 @@ error:
  * getting broken up into more disjuncts.
  *
  * After the updated domains have been computed, the result is constructed
- * from "pw1", "pw2", "list1" and "list2".  If there are any pieces
+ * from "pw1", "pw2", data[0].cell and data[1].cell.  If there are any pieces
  * in "pw1" and "pw2" with the same function value expression, then
  * they are combined into a single piece in the result.
  * In order to be able to do this efficiently, the pieces of "pw1" and
@@ -183,11 +196,11 @@ static __isl_give PW *FN(PW,union_opt_cmp)(
 	__isl_take PW *pw1, __isl_take PW *pw2,
 	__isl_give isl_set *(*cmp)(__isl_take EL *el1, __isl_take EL *el2))
 {
+	S(PW,union_opt_cmp_data) data[2] = { { pw1, NULL }, { pw2, NULL } };
 	int i, j;
 	isl_size n1, n2;
 	PW *res = NULL;
 	isl_ctx *ctx;
-	isl_set_list *list1 = NULL, *list2 = NULL;
 
 	if (!pw1 || !pw2)
 		goto error;
@@ -207,14 +220,13 @@ static __isl_give PW *FN(PW,union_opt_cmp)(
 		return pw1;
 	}
 
-	pw1 = FN(PW,sort)(pw1);
-	pw2 = FN(PW,sort)(pw2);
+	for (i = 0; i < 2; ++i) {
+		data[i].pw = FN(PW,sort)(data[i].pw);
+		data[i].cell = FN(PW,extract_domains)(data[i].pw);
+	}
 
-	list1 = FN(PW,extract_domains)(pw1);
-	list2 = FN(PW,extract_domains)(pw2);
-
-	n1 = FN(PW,n_piece)(pw1);
-	n2 = FN(PW,n_piece)(pw2);
+	n1 = FN(PW,n_piece)(data[0].pw);
+	n2 = FN(PW,n_piece)(data[1].pw);
 	if (n1 < 0 || n2 < 0)
 		goto error;
 	for (i = 0; i < n1; ++i) {
@@ -223,35 +235,37 @@ static __isl_give PW *FN(PW,union_opt_cmp)(
 			isl_set *better, *set_i, *set_j;
 			EL *el_i, *el_j;
 
-			set_i = FN(PW,peek_domain_at)(pw1, i);
-			set_j = FN(PW,peek_domain_at)(pw2, j);
+			set_i = FN(PW,peek_domain_at)(data[0].pw, i);
+			set_j = FN(PW,peek_domain_at)(data[1].pw, j);
 			disjoint = isl_set_is_disjoint(set_i, set_j);
 			if (disjoint < 0)
 				goto error;
 			if (disjoint)
 				continue;
-			el_i = FN(PW,peek_base_at)(pw1, i);
-			el_j = FN(PW,peek_base_at)(pw2, j);
+			el_i = FN(PW,peek_base_at)(data[0].pw, i);
+			el_j = FN(PW,peek_base_at)(data[1].pw, j);
 			better = FN(PW,better)(el_j, el_i, cmp);
-			set_i = isl_set_list_get_set(list1, i);
-			set_j = FN(PW,get_domain_at)(pw2, j);
+			set_i = isl_set_list_get_set(data[0].cell, i);
+			set_j = FN(PW,get_domain_at)(data[1].pw, j);
 			set_i = FN(PW,worse_or_out)(set_i,
 						isl_set_copy(better), set_j);
-			list1 = isl_set_list_set_set(list1, i, set_i);
-			set_i = FN(PW,get_domain_at)(pw1, i);
-			set_j = isl_set_list_get_set(list2, j);
+			data[0].cell =
+				isl_set_list_set_set(data[0].cell, i, set_i);
+			set_i = FN(PW,get_domain_at)(data[0].pw, i);
+			set_j = isl_set_list_get_set(data[1].cell, j);
 			set_j = FN(PW,better_or_out)(set_j, better, set_i);
-			list2 = isl_set_list_set_set(list2, j, set_j);
+			data[1].cell =
+				isl_set_list_set_set(data[1].cell, j, set_j);
 		}
 	}
 
-	res = FN(PW,merge)(pw1, pw2, list1, list2);
+	res = FN(PW,merge)(&data[0], &data[1]);
+	for (i = 0; i < 2; ++i)
+		FN(PW,union_opt_cmp_data_clear)(&data[i]);
 
 	return res;
 error:
-	isl_set_list_free(list1);
-	isl_set_list_free(list2);
-	FN(PW,free)(pw1);
-	FN(PW,free)(pw2);
+	for (i = 0; i < 2; ++i)
+		FN(PW,union_opt_cmp_data_clear)(&data[i]);
 	return FN(PW,free)(res);
 }
