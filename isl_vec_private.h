0ed6b137dd04d3f1af3188f19e1ee953471c2357
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2020-03-08 14:53:01 +0100

    drop spurious isl_vec_get_element declaration
    This was missing from isl-0.18-963-gc1351d995d (drop deprecated
    isl_int interface, Wed Sep 20 14:40:03 2017 +0200).
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_vec_private.h
+++ isl_vec_private.h
@@ -20,7 +20,6 @@ uint32_t isl_vec_get_hash(__isl_keep isl_vec *vec);
 __isl_give isl_vec *isl_vec_cow(__isl_take isl_vec *vec);
 
 void isl_vec_lcm(struct isl_vec *vec, isl_int *lcm);
-int isl_vec_get_element(__isl_keep isl_vec *vec, int pos, isl_int *v);
 __isl_give isl_vec *isl_vec_set(__isl_take isl_vec *vec, isl_int v);
 
 isl_bool isl_vec_is_zero(__isl_keep isl_vec *vec);
