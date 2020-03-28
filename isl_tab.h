1d6a93d3e5dafa98b3a1976ff46c0d4a3c6421fb
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2020-03-28 14:31:43 +0100

    isl_tab_from_recession_cone: add memory management annotation
    This was missing from isl-0.02-189-g57b4c79a9aa (export
    isl_set_is_bounded, Sun May 23 16:27:32 2010 +0200).
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- isl_tab.h
+++ isl_tab.h
@@ -190,7 +190,7 @@ __isl_give struct isl_tab *isl_tab_from_basic_map(
 	__isl_keep isl_basic_map *bmap, int track);
 __isl_give struct isl_tab *isl_tab_from_basic_set(
 	__isl_keep isl_basic_set *bset, int track);
-struct isl_tab *isl_tab_from_recession_cone(struct isl_basic_set *bset,
+struct isl_tab *isl_tab_from_recession_cone(__isl_keep isl_basic_set *bset,
 	int parametric);
 isl_bool isl_tab_cone_is_bounded(struct isl_tab *tab);
 struct isl_basic_map *isl_basic_map_update_from_tab(struct isl_basic_map *bmap,
