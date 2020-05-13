9944f9027e5a739b75bd14ca092e259ebf4e1e7a
Sven Verdoolaege <sven@cerebras.net>
2020-05-13 12:28:21 +0200

    export isl_{,union_}map_intersect_*_factor_*
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- include/isl/union_map.h
+++ include/isl/union_map.h
@@ -164,12 +164,16 @@ __isl_give isl_union_map *isl_union_map_intersect_range_space(
 	__isl_take isl_union_map *umap, __isl_take isl_space *space);
 __isl_give isl_union_map *isl_union_map_intersect_range(
 	__isl_take isl_union_map *umap, __isl_take isl_union_set *uset);
+__isl_export
 __isl_give isl_union_map *isl_union_map_intersect_domain_factor_domain(
 	__isl_take isl_union_map *umap, __isl_take isl_union_map *factor);
+__isl_export
 __isl_give isl_union_map *isl_union_map_intersect_domain_factor_range(
 	__isl_take isl_union_map *umap, __isl_take isl_union_map *factor);
+__isl_export
 __isl_give isl_union_map *isl_union_map_intersect_range_factor_domain(
 	__isl_take isl_union_map *umap, __isl_take isl_union_map *factor);
+__isl_export
 __isl_give isl_union_map *isl_union_map_intersect_range_factor_range(
 	__isl_take isl_union_map *umap, __isl_take isl_union_map *factor);
 
