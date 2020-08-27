97f095425d8dd6e3411a87a955408994f92061f9
Sven Verdoolaege <sven@cerebras.net>
2020-08-27 17:20:38 +0200

    export isl_map_product
    This complements the already exported isl_union_map_product and
    isl_map_{domain,range}_product.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- include/isl/map.h
+++ include/isl/map.h
@@ -358,6 +358,7 @@ __isl_give isl_map *isl_map_preimage_domain_multi_pw_aff(
 	__isl_take isl_map *map, __isl_take isl_multi_pw_aff *mpa);
 __isl_give isl_basic_map *isl_basic_map_product(
 	__isl_take isl_basic_map *bmap1, __isl_take isl_basic_map *bmap2);
+__isl_export
 __isl_give isl_map *isl_map_product(__isl_take isl_map *map1,
 	__isl_take isl_map *map2);
 __isl_give isl_basic_map *isl_basic_map_domain_product(
