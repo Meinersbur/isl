9722e1ccd17fa7df7e777147b232581859470265
Sven Verdoolaege <sven@cerebras.net>
2021-06-23 15:24:16 +0200

    isl_pw_*_insert_dims: use isl_pw_*_n_piece
    This reduces the dependence on the internal representation.
    
    Signed-off-by: Sven Verdoolaege <sven@cerebras.net>

--- isl_pw_insert_dims_templ.c
+++ isl_pw_insert_dims_templ.c
@@ -12,10 +12,12 @@ __isl_give PW *FN(PW,insert_dims)(__isl_take PW *pw, enum isl_dim_type type,
 	unsigned first, unsigned n)
 {
 	int i;
+	isl_size n_piece;
 	enum isl_dim_type set_type;
 
-	if (!pw)
-		return NULL;
+	n_piece = FN(PW,n_piece)(pw);
+	if (n_piece < 0)
+		return FN(PW,free)(pw);
 	if (n == 0 && !isl_space_is_named_or_nested(pw->dim, type))
 		return pw;
 
@@ -29,7 +31,7 @@ __isl_give PW *FN(PW,insert_dims)(__isl_take PW *pw, enum isl_dim_type type,
 	if (!pw->dim)
 		goto error;
 
-	for (i = 0; i < pw->n; ++i) {
+	for (i = 0; i < n_piece; ++i) {
 		pw->p[i].set = isl_set_insert_dims(pw->p[i].set,
 							    set_type, first, n);
 		if (!pw->p[i].set)
