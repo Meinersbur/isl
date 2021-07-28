90c72afbdf1dc14b27023a07dee63877c17034ad
Riccardo Mori <patacca@autistici.org>
2021-07-28 20:50:54 +0200

    Export isl_pw_aff_var_on_domain

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -224,6 +224,7 @@ __isl_give isl_pw_aff *isl_pw_aff_alloc(__isl_take isl_set *set,
 	__isl_take isl_aff *aff);
 __isl_give isl_pw_aff *isl_pw_aff_zero_on_domain(
 	__isl_take isl_local_space *ls);
+__isl_export
 __isl_give isl_pw_aff *isl_pw_aff_var_on_domain(__isl_take isl_local_space *ls,
 	enum isl_dim_type type, unsigned pos);
 __isl_give isl_pw_aff *isl_pw_aff_nan_on_domain_space(
