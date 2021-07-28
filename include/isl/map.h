9a3b17850a550a0cc4d555efbd49dc11d9b2bf1e
Riccardo Mori <patacca@autistici.org>
2021-07-28 21:37:44 +0200

    Export isl_map_gist_params
    The function was already exported but now we are exporting the function that returns an isl_map

--- include/isl/map.h
+++ include/isl/map.h
@@ -688,6 +688,7 @@ __isl_give isl_map *isl_map_gist_domain(__isl_take isl_map *map,
 	__isl_take isl_set *context);
 __isl_give isl_map *isl_map_gist_range(__isl_take isl_map *map,
 	__isl_take isl_set *context);
+__isl_export
 __isl_give isl_map *isl_map_gist_params(__isl_take isl_map *map,
 	__isl_take isl_set *context);
 __isl_give isl_map *isl_map_gist_basic_map(__isl_take isl_map *map,
