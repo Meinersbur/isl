a6ed9b34b5cfc4d84236535c115b52928c694f29
Riccardo Mori <patacca@autistici.org>
2021-07-28 20:53:41 +0200

    Export isl_pw_aff_set_tuple_id

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -295,6 +295,7 @@ isl_bool isl_pw_aff_has_tuple_id(__isl_keep isl_pw_aff *pa,
 	enum isl_dim_type type);
 __isl_give isl_id *isl_pw_aff_get_tuple_id(__isl_keep isl_pw_aff *pa,
 	enum isl_dim_type type);
+__isl_export
 __isl_give isl_pw_aff *isl_pw_aff_set_tuple_id(__isl_take isl_pw_aff *pwaff,
 	enum isl_dim_type type, __isl_take isl_id *id);
 __isl_give isl_pw_aff *isl_pw_aff_reset_tuple_id(__isl_take isl_pw_aff *pa,
