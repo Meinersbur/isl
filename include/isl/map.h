4a9237e51033b4402c8e32e2735b76227ac268c1
Riccardo Mori <patacca@autistici.org>
2021-07-28 20:30:15 +0200

    Export isl_map_from_domain_and_range

--- include/isl/map.h
+++ include/isl/map.h
@@ -573,6 +573,7 @@ __isl_give isl_basic_map *isl_basic_map_from_range(
 __isl_give isl_map *isl_map_from_range(__isl_take isl_set *set);
 __isl_give isl_basic_map *isl_basic_map_from_domain_and_range(
 	__isl_take isl_basic_set *domain, __isl_take isl_basic_set *range);
+__isl_export
 __isl_give isl_map *isl_map_from_domain_and_range(__isl_take isl_set *domain,
 	__isl_take isl_set *range);
 __isl_export
