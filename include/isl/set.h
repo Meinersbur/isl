4fdfc14847207db1ec0a6ef052ce9b5cde8121cb
Riccardo Mori <patacca@autistici.org>
2021-07-28 20:49:34 +0200

    Export isl_basic_set_universe

--- include/isl/set.h
+++ include/isl/set.h
@@ -98,6 +98,7 @@ int isl_basic_set_is_rational(__isl_keep isl_basic_set *bset);
 __isl_null isl_basic_set *isl_basic_set_free(__isl_take isl_basic_set *bset);
 __isl_give isl_basic_set *isl_basic_set_copy(__isl_keep isl_basic_set *bset);
 __isl_give isl_basic_set *isl_basic_set_empty(__isl_take isl_space *space);
+__isl_export
 __isl_give isl_basic_set *isl_basic_set_universe(__isl_take isl_space *space);
 __isl_give isl_basic_set *isl_basic_set_nat_universe(
 	__isl_take isl_space *space);
