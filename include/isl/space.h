af17a86b09d38ab6b4429702198e23c729e4b71a
Riccardo Mori <patacca@autistici.org>
2021-07-28 20:29:38 +0200

    Export isl_space_dim

--- include/isl/space.h
+++ include/isl/space.h
@@ -203,6 +203,7 @@ isl_bool isl_space_tuple_is_equal(__isl_keep isl_space *space1,
 ISL_DEPRECATED
 isl_bool isl_space_match(__isl_keep isl_space *space1, enum isl_dim_type type1,
 	__isl_keep isl_space *space2, enum isl_dim_type type2);
+__isl_export
 isl_size isl_space_dim(__isl_keep isl_space *space, enum isl_dim_type type);
 
 __isl_export
