38bc1ef375034c0b7a4e46da2b76842422ed2da6
Riccardo Mori <patacca@autistici.org>
2021-07-28 21:39:07 +0200

    Export isl_set_reset_tuple_id

--- include/isl/set.h
+++ include/isl/set.h
@@ -83,6 +83,7 @@ __isl_give isl_id *isl_set_get_dim_id(__isl_keep isl_set *set,
 	enum isl_dim_type type, unsigned pos);
 __isl_give isl_set *isl_set_set_tuple_id(__isl_take isl_set *set,
 	__isl_take isl_id *id);
+__isl_export
 __isl_give isl_set *isl_set_reset_tuple_id(__isl_take isl_set *set);
 isl_bool isl_set_has_tuple_id(__isl_keep isl_set *set);
 __isl_give isl_id *isl_set_get_tuple_id(__isl_keep isl_set *set);
