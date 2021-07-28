273bd6700b38ecca80bf1e688ffff7086af7ded8
Riccardo Mori <patacca@autistici.org>
2021-07-28 21:28:02 +0200

    Export isl_map_sum

--- include/isl/map.h
+++ include/isl/map.h
@@ -232,6 +232,7 @@ __isl_give isl_basic_map *isl_basic_map_sum(__isl_take isl_basic_map *bmap1,
 	__isl_take isl_basic_map *bmap2);
 __isl_give isl_basic_map *isl_basic_map_neg(__isl_take isl_basic_map *bmap);
 
+__isl_export
 __isl_give isl_map *isl_map_sum(__isl_take isl_map *map1,
 	__isl_take isl_map *map2);
 __isl_give isl_map *isl_map_neg(__isl_take isl_map *map);
