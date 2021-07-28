7455485c6c2063366c6447a5bf65a855c714d5f3
Riccardo Mori <patacca@autistici.org>
2021-07-28 21:44:49 +0200

    Export isl_map_from_aff

--- include/isl/map.h
+++ include/isl/map.h
@@ -782,6 +782,7 @@ __isl_give isl_basic_map *isl_basic_map_from_multi_aff(
 __isl_give isl_basic_map *isl_basic_map_from_aff_list(
 	__isl_take isl_space *domain_space, __isl_take isl_aff_list *list);
 
+__isl_export
 __isl_give isl_map *isl_map_from_aff(__isl_take isl_aff *aff);
 __isl_export
 __isl_give isl_map *isl_multi_aff_as_map(__isl_take isl_multi_aff *ma);
