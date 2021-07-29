eca142b5cf55bf9b9f6a862bce2ef48a4b4fbeda
Riccardo Mori <patacca@autistici.org>
2021-07-29 13:46:26 +0200

    Export constructor isl_multi_union_pw_aff_from_union_pw_multi_aff

--- include/isl/aff.h
+++ include/isl/aff.h
@@ -1442,6 +1442,7 @@ __isl_export
 __isl_give isl_multi_union_pw_aff *
 isl_union_pw_multi_aff_as_multi_union_pw_aff(
 	__isl_take isl_union_pw_multi_aff *upma);
+__isl_constructor
 __isl_give isl_multi_union_pw_aff *
 isl_multi_union_pw_aff_from_union_pw_multi_aff(
 	__isl_take isl_union_pw_multi_aff *upma);
