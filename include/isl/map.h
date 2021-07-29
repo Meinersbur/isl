c0a47390715238e0f30b0ccaf77fa0aaf137bc22
Riccardo Mori <patacca@autistici.org>
2021-07-29 19:22:03 +0200

    Export isl_basic_map_equal

--- include/isl/map.h
+++ include/isl/map.h
@@ -123,6 +123,7 @@ isl_bool isl_basic_map_is_rational(__isl_keep isl_basic_map *bmap);
 __isl_give isl_basic_map *isl_basic_map_identity(__isl_take isl_space *space);
 __isl_null isl_basic_map *isl_basic_map_free(__isl_take isl_basic_map *bmap);
 __isl_give isl_basic_map *isl_basic_map_copy(__isl_keep isl_basic_map *bmap);
+__isl_export
 __isl_give isl_basic_map *isl_basic_map_equal(
 	__isl_take isl_space *space, unsigned n_equal);
 __isl_give isl_basic_map *isl_basic_map_less_at(__isl_take isl_space *space,
