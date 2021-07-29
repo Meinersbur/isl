e7175ba864178feaa4fa78898b0a4873bdef26e0
Riccardo Mori <patacca@autistici.org>
2021-07-29 19:16:23 +0200

    Export isl_set_remove_redundancies

--- include/isl/set.h
+++ include/isl/set.h
@@ -143,6 +143,7 @@ __isl_give isl_basic_set *isl_basic_set_detect_equalities(
 						__isl_take isl_basic_set *bset);
 __isl_give isl_basic_set *isl_basic_set_remove_redundancies(
 	__isl_take isl_basic_set *bset);
+__isl_export
 __isl_give isl_set *isl_set_remove_redundancies(__isl_take isl_set *set);
 __isl_give isl_basic_set *isl_basic_set_list_intersect(
 	__isl_take struct isl_basic_set_list *list);
