388f51621823f628479ad63a19d9e55b713aa7a0
Riccardo Mori <patacca@autistici.org>
2021-07-29 18:09:15 +0200

    Export isl_basic_set_fix_val

--- include/isl/set.h
+++ include/isl/set.h
@@ -166,6 +166,7 @@ __isl_give isl_printer *isl_printer_print_set(__isl_take isl_printer *printer,
 __isl_export
 __isl_give isl_basic_set *isl_basic_set_fix_si(__isl_take isl_basic_set *bset,
 		enum isl_dim_type type, unsigned pos, int value);
+__isl_export
 __isl_give isl_basic_set *isl_basic_set_fix_val(__isl_take isl_basic_set *bset,
 	enum isl_dim_type type, unsigned pos, __isl_take isl_val *v);
 __isl_export
