10549f1646ed2b4b7f646d1ca8672be473826003
Riccardo Mori <patacca@autistici.org>
2021-07-29 18:51:24 +0200

    Export isl_set_drop_constraints_involving_dims

--- include/isl/set.h
+++ include/isl/set.h
@@ -443,6 +443,7 @@ __isl_give isl_basic_set *isl_basic_set_drop_constraints_involving_dims(
 __isl_give isl_basic_set *isl_basic_set_drop_constraints_not_involving_dims(
 	__isl_take isl_basic_set *bset,
 	enum isl_dim_type type, unsigned first, unsigned n);
+__isl_export
 __isl_give isl_set *isl_set_drop_constraints_involving_dims(
 	__isl_take isl_set *set,
 	enum isl_dim_type type, unsigned first, unsigned n);
