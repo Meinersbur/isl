ca15a1614704450ff105c261a16fc3b440bef403
Riccardo Mori <patacca@autistici.org>
2021-07-29 19:07:56 +0200

    Export isl_map_dim_max

--- include/isl/map.h
+++ include/isl/map.h
@@ -810,6 +810,7 @@ __isl_give isl_map *isl_map_from_multi_aff(__isl_take isl_multi_aff *maff);
 
 __isl_export
 __isl_give isl_pw_aff *isl_map_dim_min(__isl_take isl_map *map, int pos);
+__isl_export
 __isl_give isl_pw_aff *isl_map_dim_max(__isl_take isl_map *map, int pos);
 
 ISL_DECLARE_EXPORTED_LIST_FN(basic_map)
