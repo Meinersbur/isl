995339b336f7eb32582f77db8a6405536f4190be
Riccardo Mori <patacca@autistici.org>
2021-07-29 16:05:47 +0200

    Export isl_set_set_dim_id

--- include/isl/set.h
+++ include/isl/set.h
@@ -79,6 +79,7 @@ __isl_give isl_id *isl_basic_set_get_dim_id(__isl_keep isl_basic_set *bset,
 	enum isl_dim_type type, unsigned pos);
 __isl_give isl_basic_set *isl_basic_set_set_tuple_id(
 	__isl_take isl_basic_set *bset, __isl_take isl_id *id);
+__isl_export
 __isl_give isl_set *isl_set_set_dim_id(__isl_take isl_set *set,
 	enum isl_dim_type type, unsigned pos, __isl_take isl_id *id);
 isl_bool isl_set_has_dim_id(__isl_keep isl_set *set,
