6ecd4386be702065f9c903cd7257fcb618dbf159
Riccardo Mori <patacca@autistici.org>
2021-07-29 17:15:55 +0200

    Export isl_set_dim_max

--- include/isl/set.h
+++ include/isl/set.h
@@ -615,6 +615,7 @@ __isl_give isl_basic_set *isl_basic_set_solutions(
 	__isl_take isl_basic_set *bset);
 __isl_give isl_basic_set *isl_set_solutions(__isl_take isl_set *set);
 
+__isl_export
 __isl_give isl_pw_aff *isl_set_dim_max(__isl_take isl_set *set, int pos);
 __isl_export
 __isl_give isl_pw_aff *isl_set_dim_min(__isl_take isl_set *set, int pos);
